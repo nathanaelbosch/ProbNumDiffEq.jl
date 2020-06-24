2574949d98df923570b51660693bb09cf3acfbf0
Nathanael Bosch <nathanael.bosch@gmail.de>
2020-06-24 18:44:41 +0200

    Added preconditioning

--- src/algorithm.jl
+++ src/algorithm.jl
@@ -37,26 +37,30 @@ requires a different order of comutation of the update and predict steps.
 function predict_update(solver, cache)
     @unpack dm, mm = solver
     @unpack t, x, dt = cache
+    precond_P = solver.preconditioner.P
+    precond_P_inv = solver.preconditioner.P_inv
+
     t = t + dt
 
     m, P = x.μ, x.Σ
     A, Q = dm.A(dt), dm.Q(dt)
+    A = precond_P * A * precond_P_inv
+    Q = precond_P * Q * precond_P'
 
     m_p = A * m
 
-    H, R = mm.H(m_p, t), mm.R
-    v = 0 .- mm.h(m_p, t)
+    # H, R = mm.H(m_p, t), mm.R
+    H, R = mm.H(m_p, t) * precond_P_inv, mm.R
+    # v = 0 .- mm.h(m_p, t)
+    v = 0 .- mm.h(precond_P_inv * m_p, t)
 
     # Decide if constant sigma or variable sigma
     # σ² = solver.sigma_type == :fixed ? 1 :
     #     solver.sigma_estimator(;H=H, Q=Q, v=v)
     σ² = dynamic_sigma_estimation(solver.sigma_estimator; H=H, Q=Q, v=v)
 
-    # @show P
     P_p = A*P*A' + σ²*Q
     S = H * P_p * H' + R
-    # @show P_p
-    # @show S
     K = P_p * H' * inv(S)
     m = m_p + K*v
     P = P_p - K*S*K'
@@ -83,26 +87,35 @@ function smooth(filter_estimate::Gaussian,
 end
 
 function smooth(sol, solver, proposals)
+    precond_P = solver.preconditioner.P
+    precond_P_inv = solver.preconditioner.P_inv
+
+
     smoothed_solution = StructArray{StateBelief}(undef, length(sol))
     smoothed_solution[end] = sol[end]
     smoothed_solution[1] = sol[1]
     accepted_proposals = [p for p in proposals if p.accept]
-    @show length(accepted_proposals), length(sol)
     for i in length(smoothed_solution)-1:-1:2
         h = accepted_proposals[i].dt  # step t -> t+1
         h2 = sol[i+1].t - sol[i].t
         @assert h ≈ h2
 
+        @assert accepted_proposals[i].t == sol[i+1].t
+
         prediction = accepted_proposals[i].prediction  # t+1
         filter_estimate = sol[i].x  # t
         smoothed_estimate = smoothed_solution[i+1].x # t+1
 
+        A = solver.dm.A(h)
+        Q = solver.dm.Q(h)
+        A = precond_P * A * precond_P_inv
+        Q = precond_P * Q * precond_P'
         smoothed_solution[i] = StateBelief(
             sol[i].t,
             smooth(filter_estimate,
                    prediction,
                    smoothed_estimate,
-                   (A=solver.dm.A(h), Q=solver.dm.Q(h)))
+                   (A=A, Q=Q))
         )
     end
     return smoothed_solution
@@ -142,6 +155,23 @@ function ibm(q::Int, d::Int; σ::Int=1)
     return (A=A, Q=Q, q=q, d=d)
 end
 
+function preconditioner(expected_stepsize, d, q)
+    h = expected_stepsize
+    I_d = diagm(0 => ones(d))
+    P = Diagonal(kron(Diagonal(h .^ 0:(q+1)), I_d))
+    P_inv = Diagonal(kron(Diagonal(1 ./ (h .^ 0:(q+1))), I_d))
+    return (P=P, P_inv=P_inv)
+    # return (P=I, P_inv=I)
+end
+function apply_preconditioner!(p, x::Gaussian)
+    x.μ .= p.P * x.μ
+    x.Σ .= Symmetric(p.P * x.Σ * p.P')
+end
+function undo_preconditioner!(p, x::Gaussian)
+    x.μ .= p.P_inv * x.μ
+    x.Σ .= Symmetric(p.P_inv * x.Σ * p.P_inv')
+end
+
 
 # Measurement Model
 function ekf0_measurement_model(d, q, ivp)
@@ -176,6 +206,7 @@ Base.@kwdef struct Solver
     dm                          # Dynamics Model
     mm                          # Measurement Model
     sigma_estimator
+    preconditioner
 end
 
 Base.@kwdef mutable struct SolverCache
@@ -238,6 +269,10 @@ function initialize(;ivp, q, dt, σ, method, sigmarule, initialize_derivatives)
     P_0 = diagm(0 => [zeros(d); ones(d*q)] .+ 1e-16)
     initial_state = Gaussian(m_0, P_0)
 
+    # Precondition
+    precond = preconditioner(h, d, q)
+    apply_preconditioner!(precond, initial_state)
+
     # sigmarules = Dict(
     #     :mle => sigma_mle,
     #     :mle_weighted => sigma_mle_weighted,
@@ -250,6 +285,7 @@ function initialize(;ivp, q, dt, σ, method, sigmarule, initialize_derivatives)
 
     return (Solver(;d=d, q=q, dm=dm, mm=mm,
                    sigma_estimator=sigma_estimator,
+                   preconditioner=precond,),
             SolverCache(;t=t_0, x=initial_state, dt=h))
 end
 
@@ -333,6 +369,16 @@ function prob_solve(ivp, dt;
             p.filter_estimate.Σ *= σ²
         end
     end
+
+    # Undo preconditioning
+    for s in sol
+        undo_preconditioner!(solver.preconditioner, s.x)
+    end
+    for p in proposals
+        undo_preconditioner!(solver.preconditioner, p.prediction)
+        undo_preconditioner!(solver.preconditioner, p.filter_estimate)
+    end
+
 
     return (
         prob=ivp,
