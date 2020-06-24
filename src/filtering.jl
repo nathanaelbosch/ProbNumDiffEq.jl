5147fdd4839562354dd72c3098e63e7932f0dba0
Nathanael Bosch <nathanael.bosch@gmail.de>
2020-06-24 18:35:54 +0200

    Add 1e-16 to covariances to make smoothing more stable

--- src/filtering.jl
+++ src/filtering.jl
@@ -14,6 +14,12 @@ function kf_smooth(m_f_t, P_f_t, m_p_t1, P_p_t1, m_s_t1, P_s_t1, A, Q)
     G = P_f_t * A' * inv(P_p_t1)
     m = m_f_t + G * (m_s_t1 - m_p_t1)
     P = P_f_t + G * (P_s_t1 - P_p_t1) * G'
+    _min = minimum(diag(P))
+    if _min < 0
+        @assert abs(_min) < 1e-16
+        P .+= -_min
+    end
+    @assert all(diag(P) .>= 0)
     return m, P
 end
 
