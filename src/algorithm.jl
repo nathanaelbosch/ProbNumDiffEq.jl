6862714963d0e76f068cf4aba952e84ebd695f3c
Nathanael Bosch <nathanael.bosch@tum.de>
2020-06-28 19:02:41 +0200

    Changed constructors for the measurement models

--- src/algorithm.jl
+++ src/algorithm.jl
@@ -175,29 +175,35 @@ end
 
 
 # Measurement Model
-function ekf0_measurement_model(d, q, ivp)
+function ekf0_measurement_model(d, q, f, p)
     H_0 = kron([i==1 ? 1 : 0 for i in 1:q+1]', diagm(0 => ones(d)))
     H_1 = kron([i==2 ? 1 : 0 for i in 1:q+1]', diagm(0 => ones(d)))
     R = zeros(d, d)
 
-    h(m, t) = H_1*m - ivp.f(H_0*m, ivp.p, t)
+    h(m, t) = H_1*m - f(H_0*m, p, t)
     H(m, t) = H_1
     return (h=h, H=H, R=R)
+end
+function ekf0_measurement_model(d, q, ivp)
+    return ekf0_measurement_model(d, q, ivp.f, ivp.p)
 end
 
 
 # Measurement Model
-function ekf1_measurement_model(d, q, ivp)
+function ekf1_measurement_model(d, q, f, p, kwargs)
     H_0 = kron([i==1 ? 1 : 0 for i in 1:q+1]', diagm(0 => ones(d)))
     H_1 = kron([i==2 ? 1 : 0 for i in 1:q+1]', diagm(0 => ones(d)))
     R = zeros(d, d)
 
-    Jf(x, p, t) = (:jac in keys(ivp.kwargs)) ?
-        ivp.kwargs[:jac](x, ivp.p, t) :
-        ForwardDiff.jacobian(_u -> ivp.f(_u, ivp.p, t), x)
-    h(m, t) = H_1*m - ivp.f(H_0*m, ivp.p, t)
-    H(m, t) = H_1 - Jf(H_0*m, ivp.p, t) * H_0
+    Jf(x, p, t) = (:jac in keys(kwargs)) ?
+        kwargs[:jac](x, p, t) :
+        ForwardDiff.jacobian(_u -> f(_u, p, t), x)
+    h(m, t) = H_1*m - f(H_0*m, p, t)
+    H(m, t) = H_1 - Jf(H_0*m, p, t) * H_0
     return (h=h, H=H, R=R)
+end
+function ekf1_measurement_model(d, q, ivp)
+    return ekf1_measurement_model(d, q, ivp.f, ivp.p, ivp.kwargs)
 end
 
 
