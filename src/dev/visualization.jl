80464b71257aa875b9b95cbacad61f5090f91be4
Nathanael Bosch <nathanael.bosch@gmail.de>
2020-06-29 12:09:05 +0200

    Plots are now (mostly) plot recipes :)

--- src/dev/visualization.jl
+++ src/dev/visualization.jl
@@ -1,69 +1,90 @@
-function plot_solution(sol, derivative=0; true_solution=nothing, stdfac=1.96)
-    stds = hcat([sqrt.(diag(M)) for M in sol.u.Σ]...)'
+@userplot Plot_Solution
+@recipe function f(ps::Plot_Solution; derivative=0, true_solution=nothing, stdfac=1.96)
+    sol = ps.args[1]
+
+    stds = hcat([sqrt.(diag(M)) for M in sol.x.Σ]...)'
     d = sol.solver.d
 
     # Plot the results
-    means = hcat(sol.u.μ...)'
-    p1 = plot(sol.t, means[:, 1:d],
-              ribbon=stdfac*stds[:, 1:d],
-              marker=:x,
-              title="\$f(t)\$", label="Filter Estimate", xlabel="t")
-    if !(isnothing(true_solution))
-        plot!(p1, range(sol.t[1], sol.t[end], length=1000), true_solution, label="True solution")
-    end
-    return p1
+    means = hcat(sol.x.μ...)'
+
+    # return p1
+    ribbon --> stdfac*stds[:, 1:d]
+    # marker --> :x
+    title --> "\$f(t)\$"
+    label --> "Filter Estimate"
+    xguide --> "t"
+
+    # TODO: Add true solution back in
+    # if !(isnothing(true_solution))
+    #     plot!(p1, range(sol.t[1], sol.t[end], length=1000), true_solution, label="True solution")
+    # end
+
+    return sol.t, means[:, 1:d]
 end
 
 
-function plot_stepsizes!(p, sol; rejections=true, argv...)
+@userplot Plot_Stepsizes
+@recipe function f(ps::Plot_Stepsizes; rejections=true)
+    sol = ps.args[1]
+
     stepsizes = sol.t[2:end-1] - sol.t[1:end-2]
     if all([h ≈ stepsizes[1] for h in stepsizes])
         stepsizes .= stepsizes[1]
     end
-    # @show argv
-    pargv = (yscale=:log10,
-             # marker=:x,
-             label="h (left)",
-             ylabel="h",
-             legend=:bottomleft,
-             argv...)
-    plot!(p, sol.t[2:end-1], stepsizes; pargv...)
+
+    yscale --> :log10,
+    # marker --> :x
+    label --> "h (left)"
+    yguide --> "h"
+    legend --> :bottomleft
+
+    @series begin
+        return sol.t[2:end-1], stepsizes
+    end
+
+    # TODO Add rejections back in
     rejected = filter(p->!p.accept, sol.proposals)
     if rejections && length(rejected) > 0
-        t = [p.t for p in rejected]
-        dt = [p.dt for p in rejected]
-        scatter!(p, t, dt,
-                 marker=:x, color=:black, label="rejected")
+        @series begin
+            t = [p.t for p in rejected]
+            dt = [p.dt for p in rejected]
+            marker := :x
+            seriescolor := :black
+            label := "rejected"
+            seriestype := :scatter
+            return t, dt
+        end
     end
-    return p
 end
-plot_stepsizes(sol; argv...) = plot_stepsizes!(plot(), sol; argv...)
 
 
-function plot_analytic_solution!(p, sol, analytic)
+@userplot Plot_Analytic_Solution
+@recipe function f(pas::Plot_Analytic_Solution)
+    sol, analytic = pas.args[1], pas.args[2]
     ts = sol.t
     highres_ts = range(ts[1], ts[end], length=1000)
-    plot!(p, highres_ts, hcat(analytic.(highres_ts)...)', label="True solution")
+    label --> "True solution"
+    return highres_ts, hcat(analytic.(highres_ts)...)'
 end
 
 
-function plot_errors!(p, sol, analytic; c=1.96, argv...)
+@userplot Plot_Errors
+@recipe function f(pe::Plot_Errors; c=1.96)
+    sol, analytic = pe.args[1], pe.args[2]
     ts = sol.t
 
-    f_est = [m[1:sol.solver.d] for m in sol.u.μ]
+    f_est = [m[1:sol.solver.d] for m in sol.x.μ]
     if sol.solver.d == 1
         f_est = hcat(f_est...)'
     end
     true_sol = analytic.(ts)
     diffs = true_sol .- f_est
 
-    pargv = (yscale=:log10,
-             # marker=:x,
-             label="Global Error",
-             ylabel="function error",
-             argv...)
-    plot!(p, ts[2:end], norm.(diffs, 2)[2:end];
-          pargv...)
+    yscale --> :log10
+    # marker --> :x
+    label --> "Global Error"
+    yguide --> "function error"
 
     # local_errors = norm.([(diffs[i] - diffs[i-1]) for i in 2:length(diffs)], 2)
     # plot!(p_err, ts[2:end], local_errors, marker=:x, label="Local Error")
@@ -73,18 +94,18 @@ function plot_errors!(p, sol, analytic; c=1.96, argv...)
     #       linestyle=:dash,
     #       (argv..., labels="$c*std")...)
 
-    return p
+    return ts[2:end], norm.(diffs, 2)[2:end]
 end
-plot_errors(sol, analytic; argv...) = plot_errors!(plot(), sol, analytic; argv...)
 
 
-stack(x) = copy(reduce(hcat, x)')
-function plot_calibration!(p, sol, analytic; label="")
+@userplot Plot_Calibration
+@recipe function f(pc::Plot_Calibration; interval=0.95)
+    sol, analytic = pc.args[1], pc.args[2]
     ts = sol.t
     d = sol.solver.d
 
-    f_est = [m[1:d] for m in sol.u.μ]
-    f_covs = [u.Σ[1:d, 1:d] for u in sol.u]
+    f_est = [m[1:d] for m in sol.x.μ]
+    f_covs = [u.Σ[1:d, 1:d] for u in sol.x]
     if sol.solver.d == 1
         f_est = stack(f_est)
     end
@@ -92,36 +113,58 @@ function plot_calibration!(p, sol, analytic; label="")
     diffs = true_sol .- f_est
 
     sugg = [d' * inv(C) * d for (d, C) in zip(diffs, f_covs)]
-    plot!(p, ts[2:end], sugg[2:end],
-          label=label,
-          yscale=:log10,
-          )
-    return p
+
+    @series begin
+        label --> ""
+        yscale --> :log10
+        return ts[2:end], sugg[2:end]
+    end
+    if !isnothing(interval)
+        @series begin
+            seriestype := :hline
+            seriescolor := :black
+            linestyle := :dash
+            label := "$(interval*100)% Interval"
+            p = 1-interval
+            return [quantile(Chisq(sol.solver.d), 1-(p/2)), quantile(Chisq(sol.solver.d), p/2)]
+        end
+    end
 end
 
 
-function plot_residuals(sol)
+@userplot Plot_Residuals
+@recipe function f(pr::Plot_Residuals)
+    sol = pr.args[1]
+
     accepted_proposals = [p for p in sol.proposals if p.accept]
     measurements = [p.measurement for p in accepted_proposals]
     times = [p.t for p in accepted_proposals]
     res = [z.μ' * z.Σ^(-1) * z.μ for z in measurements] ./ sol.solver.d
-    yscale = all(res .> 0) ? :log10 : :identity
 
-    p_res = plot(times, res,
-                 marker=:x,
-                 ylabel="residual",
-                 label="residual",
-                 yscale=yscale,
-                 )
-    plot!(p_res, [1], seriestype=:hline, color=3, label=nothing)
+    yscale = all(res .> 0) ? :log10 : :identity
+    yscale --> yscale
+    yguide --> "residual"
+
+    @series begin
+        # marker --> :x
+        label --> "residual"
+        return times, res
+    end
+    @series begin
+        seriestype := :hline
+        seriescolor := 3
+        label := nothing
+        return [1]
+    end
 
     # Plot std of measurements
     msmnt_vars = [diag(m.Σ) for m in measurements]
     msmnt_stds = [norm(sqrt.(S)) for S in msmnt_vars]
-    plot!(p_res, times, 3msmnt_stds,
-          label="Measurement std",
-          marker=:x, color=2)
-
+    @series begin
+        label := "Measurement std"
+        seriescolor := 2
+        times, 3msmnt_stds
+    end
 
     # Plot rejected proposals
     rejected_proposals = [p for p in sol.proposals if !p.accept]
@@ -131,16 +174,22 @@ function plot_residuals(sol)
         res = [z.μ' * z.Σ^(-1) * z.μ for z in measurements] ./ sol.solver.d
         steps = [p.dt for p in rejected_proposals]
         # rejected_t, rejected_h, rejected_errors = collect(zip(sol.rejected...))
-        scatter!(p_res, times .+ steps, res,
-                 marker=:x, color=:black, label="rejected")
+        @series begin
+            seriestype := :scatter
+            seriescolor := :black
+            marker := :x
+            label := "rejected"
+            return times .+ steps, res
+        end
     end
-
-    return p_res
 end
 
 
 
-function plot_sigmas!(p, sol)
+@userplot Plot_Sigmas
+@recipe function f(ps::Plot_Sigmas)
+    sol = ps.args[1]
+
     # Plot Sigmas
     accepted_proposals = [p for p in sol.proposals if p.accept]
     ts = [p.t for p in accepted_proposals]
@@ -154,9 +203,14 @@ function plot_sigmas!(p, sol)
                      for i in 1:length(accepted_proposals)]
         σ² = σ²_static
     end
-    plot!(p, ts, σ²,
-          label="σ² (right)", ylabel="σ²", color=2,
-          marker=:x, yscale=:log10, legend=:bottomright)
+
+    label --> "σ² (right)"
+    yguide --> "σ²"
+    # marker --> :x
+    yscale --> :log10
+    legend --> :bottomright
+
+    return ts, σ²
 end
 
 
