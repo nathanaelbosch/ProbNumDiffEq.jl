2c7a4457c8f50b74291d9dd66d98f8e2063c0458
Nathanael Bosch <nathanael.bosch@gmail.de>
2020-06-29 09:40:42 +0200

    Improved `src/filtering.jl`

--- src/filtering.jl
+++ src/filtering.jl
@@ -1,34 +1,42 @@
-
-function kf_predict(m, P, A, Q)
+########################################################################################
+# Basic Kalman Filtering
+########################################################################################
+function kf_predict(m::Vector, P::Matrix, A::Matrix, Q::Matrix)
     return (m=(A*m), P=(A*P*A' + Q))
 end
 
-function kf_update(m, P, A, Q, H, R, y)
+function kf_update(m::Vector, P::Matrix, A::Matrix, Q::Matrix, H::Matrix, R::Matrix, y::Vector)
     v = y - H*m
     S = H * P * H' + R
     K = P * H' * inv(S)
     return (m=(m + K*v), P=(P - K*S*K'))
 end
 
-function kf_smooth(m_f_t, P_f_t, m_p_t1, P_p_t1, m_s_t1, P_s_t1, A, Q)
+function kf_smooth(m_f_t::Vector, P_f_t::Matrix,
+                   m_p_t1::Vector, P_p_t1::Matrix,
+                   m_s_t1::Vector, P_s_t1::Matrix,
+                   A::Matrix, Q::Matrix)
     G = P_f_t * A' * inv(P_p_t1)
     m = m_f_t + G * (m_s_t1 - m_p_t1)
     P = P_f_t + Symmetric(G * (P_s_t1 - P_p_t1) * G')
+
+    # Sanity: Make sure that the diagonal of P is non-negative
     _min = minimum(diag(P))
     if _min < 0
         @assert abs(_min) < 1e-16
         P += - _min*I
     end
     @assert all(diag(P) .>= 0)
+
     return m, P
 end
 
 
-function ekf_predict(m, P, f, F, Q)
+function ekf_predict(m::Vector, P::Matrix, f::Function, F::Function, Q::Matrix)
     return (m=f(m), P=(F(m)*P*F(m)' + Q))
 end
 
-function ekf_update(m, P, h, H, R, y)
+function ekf_update(m::Vector, P::Matrix, h::Function, H::Function, R::Matrix, y::Vector)
     v = y - h(m)
     S = H(m) * P * H(m)' + R
     K = P * H(m)' * inv(S)
@@ -36,7 +44,10 @@ function ekf_update(m, P, h, H, R, y)
     return (m=(m + K*v), P=(P - K*S*K'))
 end
 
-function ekf_smooth(m_f_t, P_f_t, m_p_t1, P_p_t1, m_s_t1, P_s_t1, F, Q)
+function ekf_smooth(m_f_t::Vector, P_f_t::Matrix,
+                    m_p_t1::Vector, P_p_t1::Matrix,
+                    m_s_t1::Vector, P_s_t1::Matrix,
+                    F::Function, Q::Matrix)
     G = P_f_t * F(m_f_t)' * inv(P_p_t1)
     m = m_f_t + G * (m_s_t1 - m_p_t1)
     P = P_f_t + G * (P_s_t1 - P_p_t1) * G'
