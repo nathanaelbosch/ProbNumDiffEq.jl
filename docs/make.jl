d496d3d608a7930d76d5ddd535163f44d2d93129
Nathanael Bosch <nathanael.bosch@tum.de>
2020-07-11 01:04:40 +0200

    Deploy documentation to github pages

--- docs/make.jl
+++ docs/make.jl
@@ -19,6 +19,6 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/nathanaelbosch/ProbNumODE.jl"
+)
