c5bd9ef9b67a722e242b3e81729621c21b764334
Nathanael Bosch <nathanael.bosch@tum.de>
2020-08-09 18:00:58 +0200

    Dense output (prediction)

--- src/solution.jl
+++ src/solution.jl
@@ -15,6 +15,21 @@ struct ProbODESolution{T,N,uType,xType,tType,pType,P,A,S,IType} <: AbstractProbO
     retcode::Symbol
 end
 
+function make_Measurement(state, d)
+    # @assert isdiag(state.Σ[1:d,1:d]) state.Σ[1:d,1:d]
+    mean = state.μ[1:d]
+    var = diag(state.Σ)[1:d]
+
+    _min = minimum(var)
+    if _min < 0
+        @assert abs(_min) < 1e-16
+        var .+= - _min
+    end
+    @assert all(var .>= 0)
+
+    return mean .± sqrt.(var)
+end
+
 function DiffEqBase.build_solution(
     prob::DiffEqBase.AbstractODEProblem,
     alg::ODEFilter,
@@ -25,24 +40,10 @@ function DiffEqBase.build_solution(
     kwargs...)
 
     d = length(prob.u0)
-    function make_Measurement(state)
-        # @assert isdiag(state.Σ[1:d,1:d]) state.Σ[1:d,1:d]
-        mean = state.μ[1:d]
-        var = diag(state.Σ)[1:d]
-
-        _min = minimum(var)
-        if _min < 0
-            @assert abs(_min) < 1e-16
-            var .+= - _min
-        end
-        @assert all(var .>= 0)
-
-        return mean .± sqrt.(var)
-    end
-    u = map(make_Measurement, x)
+    u = map(x -> make_Measurement(x, d), x)
     # u = map(s -> s.μ[1:d], x)
 
-    interp = DiffEqBase.LinearInterpolation(t,u)
+    interp = FilteringPosterior()
 
     T = eltype(eltype(u))
     N = length((size(prob.u0)..., length(u)))
@@ -50,6 +51,36 @@ function DiffEqBase.build_solution(
     return ProbODESolution{T, N, typeof(u), typeof(x), typeof(t), typeof(proposals),
                            typeof(prob), typeof(alg), typeof(solver), typeof(interp)}(
         u, x, t, proposals, prob, alg, solver, dense, interp, retcode)
+end
+
+########################################################################################
+# Dense Output
+########################################################################################
+struct FilteringPosterior <: DiffEqBase.AbstractDiffEqInterpolation end
+DiffEqBase.interp_summary(::FilteringPosterior) = "Filtering Posterior" 
+
+"""Just extrapolates with PREDICT so far"""
+function (sol::ProbODESolution)(t::T) where T
+    if t < sol.t[1]
+        error("Invalid t")
+    end 
+    if t in sol.t
+        idx = sum(sol.t .<= t)
+        @assert sol.t[idx] == t
+        return sol.u[idx]
+    end
+    
+    # Extrapolate
+    prev_idx = sum(sol.t .<= t)
+    prev_rv = sol.x[prev_idx]
+    m, P = prev_rv.μ, prev_rv.Σ
+    h = t - sol.t[prev_idx]
+    A, Q = sol.solver.dm.A(h), sol.solver.dm.Q(h)
+    m_pred, P_pred = kf_predict(m, P, A, Q)
+
+    pred_rv = Gaussian(m_pred, P_pred)
+    d = sol.solver.d
+    return make_Measurement(pred_rv, d)
 end
 
 
