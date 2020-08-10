eb0dc7c7af7e2e7767e0ebc0bab7a7e3545dad2b
Nathanael Bosch <nathanael.bosch@tum.de>
2020-08-10 11:45:29 +0200

    Removed old and unused steprules

--- src/steprules.jl
+++ src/steprules.jl
@@ -6,37 +6,6 @@ function constant_steprule()
     end
 end
 
-
-""" Accept/reject and increase/decrease depending on the p-value of the error
-
-DISCONTINUED FOR NOW!
-"""
-function pvalue_steprule(tol)
-    counter = 0
-    function steprule(integ)
-        @unpack proposal, proposals = integ
-        @unpack d, dt = integ
-        error("p-value steprule currently broken")
-        # current_h, current_error, previous_sigma, d, argv...)
-        pval = cdf(Chisq(d), current_error / previous_sigma)
-        h_new = current_h * 0.95
-        if (pval < 1-tol)
-            counter = 0
-            accept = true
-            h_new = h_new * 1.5
-        elseif (counter > 10)
-            accept = true
-        else
-            counter += 1
-            accept = false
-            h_new = h_new * 0.5
-        end
-        return accept, h_new
-    end
-    return steprule
-end
-
-
 """Limit the function error to provided tolerances
 
 This is a /local/ approximation; At each step we assume, that the
@@ -78,75 +47,6 @@ function classic_steprule(abstol, reltol, scale=1; ρ=0.95)
 
         h_proposal = dt * ρ * (1/f_err_scaled)^(1/(2q+1))
         h_new = min(max(h_proposal, dt*0.1), dt*5)
-
-        return accept, h_new
-    end
-    return steprule
-end
-
-
-
-"""Limit the measurement error to provided tolerances
-
-This is a /local/ approximation; At each step we assume, that the
-previous step had correct results"""
-function measurement_error_steprule(;ρ=0.95, abstol=1e-6, reltol=1e-3)
-    function steprule(integ)
-        @unpack proposal, proposals = integ
-        @unpack dm, d, q, dt, t = integ
-        @unpack measurement, σ², prediction = proposal
-        # S = previous_sigma .* measurement.Σ
-
-        σ² = static_sigma_estimation(
-            integ.sigma_estimator, integ,
-            [proposals; (proposal..., accept=true, t=t, dt=dt)])
-
-        S = σ² .* measurement.Σ
-        # @assert isdiag(S)
-        z_err = sqrt.(diag(S))
-        tol = (abstol .+ (reltol * abs.(prediction.μ[1:d])))
-
-        z_err_scaled = maximum(z_err ./ tol)
-
-        accept = z_err_scaled <= 1
-        if !accept
-            # @info "Rejected h=$current_h with scaled error e=$z_err_scaled !"
-        end
-
-        h_proposal = dt * ρ * (1/z_err_scaled)^(1/(2q-1))
-        h_new = min(max(h_proposal, dt*0.1), dt*5)
-
-        return accept, h_new
-    end
-    return steprule
-end
-
-
-"""This is basically the steprule with I discussed with Filip"""
-function measurement_scaling_steprule(abstol=1, reltol=0; ρ=0.8, hmin=1e-5)
-    function steprule(integ)
-        @unpack proposal, proposals = integ
-        @unpack dm, d, q, dt = integ
-        @unpack measurement, σ², prediction = proposal
-
-        σ² = static_sigma_estimation(
-            integ.sigma_estimator, integ,
-            [proposals; (proposal..., accept=true, t=t, dt=dt)])
-        residual = measurement.μ' * inv(measurement.Σ) * measurement.μ / σ²
-        @show residual
-        @show σ²
-
-        accept = residual <= 1
-        # !accept && @info "Rejected h=$current_h with scaled error e=$calibrated_error !"
-
-        h_exp = 1
-        h_proposal = ρ * dt * (1/residual)^(1/h_exp)
-        # @show h_proposal, h_proposal/current_h, accept
-        h_new = min(max(h_proposal, dt*0.5), dt*2)
-
-        if h_new <= hmin
-            error("Step size too small")
-        end
 
         return accept, h_new
     end
