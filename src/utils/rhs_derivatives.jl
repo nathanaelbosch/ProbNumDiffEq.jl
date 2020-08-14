e527bb387de80482a93ad25960ffcc24a2c26192
Nathanael Bosch <nathanael.bosch@tum.de>
2020-08-14 17:19:27 +0200

    Added different implementation to compute initial states

--- src/utils/rhs_derivatives.jl
+++ src/utils/rhs_derivatives.jl
@@ -18,3 +18,40 @@ function get_derivatives(f, d, q)
     end
     return out
 end
+
+
+
+
+function get_init_derivatives(prob, order)
+    # Output of size order+1
+    d = length(prob.u0)
+    q = order
+    out = fill(zero(prob.u0[1]), d*(q+1))
+
+    sys = modelingtoolkitize(prob)
+    t = sys.iv()
+    @derivatives D'~t
+    u = [s(t) for s in sys.states]
+    p = [_p() for _p in sys.ps]
+
+    rhs = ModelingToolkit.rhss(sys.eqs)
+
+    substitutions = [u .=> prob.u0; p .=> prob.p; t .=> prob.tspan[1]]
+
+    out[1:d] .= u0
+
+    @btime val = substitute.(rhs, (substitutions,))
+
+    out[d+1:2d] .= [v.value for v in val]
+    u = D.(u)
+    substitutions = [D.(u) .=> val; substitutions]
+
+    for i in 2:(order)
+        rhs = ModelingToolkit.jacobian(rhs, [t])[:]
+        val = expand_derivatives.(substitute.(rhs, (substitutions,)))
+        out[(i*d)+1:(i+1)*d] .= [v.value for v in val]
+        u = D.(u)
+        substitutions = [u .=> val; substitutions]
+    end
+    return out
+end
