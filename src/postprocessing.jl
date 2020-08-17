7e441e6b219723a896dcb4e82c67b60eb18eafec
Nathanael Bosch <nathanael.bosch@tum.de>
2020-08-17 15:10:42 +0200

    Fixed the smoothing

--- src/postprocessing.jl
+++ src/postprocessing.jl
@@ -6,39 +6,41 @@ function smooth(filter_estimate::Gaussian,
                 prediction::Gaussian,
                 smoothed_estimate::Gaussian,
                 dynamics_model)
+
     m, P = kf_smooth(
         filter_estimate.μ, filter_estimate.Σ,
         prediction.μ, prediction.Σ,
         smoothed_estimate.μ, smoothed_estimate.Σ,
         dynamics_model.A, dynamics_model.Q
     )
+
     return Gaussian(m, P)
 end
 
 function smooth!(integ::ODEFilterIntegrator)
-    @warn "Smoothing does not work right now!"
-    return
+
     @unpack state_estimates, times = integ
-    proposals = integ.proposals
-    accepted_proposals = [p for p in proposals if p.accept]
+    @unpack A!, Q! = integ.constants
+    @unpack Ah, Qh, x_pred = integ.cache
+
     for i in length(state_estimates)-1:-1:2
-        h = accepted_proposals[i].dt  # step t -> t+1
         h = times[i+1] - times[i]
-        @assert h ≈ h2
 
-        @assert accepted_proposals[i].t == times[i+1]
-
-        prediction = accepted_proposals[i].prediction  # t+1
         filter_estimate = state_estimates[i]  # t
+
+        A!(Ah, h)
+        Q!(Qh, h)
+        mul!(x_pred.μ, Ah, filter_estimate.μ)
+        x_pred.Σ .= Ah * filter_estimate.Σ * Ah' .+ Qh
+
         smoothed_estimate = state_estimates[i+1] # t+1
 
-        A = integ.dm.A(h)
-        Q = integ.dm.Q(h)
-        state_estimates[i] = smooth(filter_estimate, prediction, smoothed_estimate, (A=A, Q=Q))
+        state_estimates[i] = smooth(filter_estimate, x_pred, smoothed_estimate, (A=Ah, Q=Qh))
     end
 end
 
 function calibrate!(integ::ODEFilterIntegrator)
+
     @unpack state_estimates = integ
     proposals = integ.proposals
     σ² = static_sigma_estimation(integ.sigma_estimator, integ, proposals)
