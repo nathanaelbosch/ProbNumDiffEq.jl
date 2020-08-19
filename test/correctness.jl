8e79873b920fd24293ea496361219d181d3fefb4
Nathanael Bosch <nathanael.bosch@tum.de>
2020-08-19 17:23:25 +0200

    Added tests for "correctness" of the methods

---
+++ test/correctness.jl
@@ -0,0 +1,51 @@
+# Make sure that our solvers with small steps return correct results
+# Compare with DifferentialEquations.jl.
+using Test
+using OrdinaryDiffEq
+using DiffEqProblemLibrary.ODEProblemLibrary: importodeproblems; importodeproblems()
+import DiffEqProblemLibrary.ODEProblemLibrary: prob_ode_linear, prob_ode_2Dlinear, prob_ode_lotkavoltera, prob_ode_fitzhughnagumo
+using ModelingToolkit
+
+
+
+function test_prob_solution_correctness(prob, args...; kwargs...)
+
+    prob = remake(prob, p=collect(prob.p))
+
+    sol = solve(prob, args...; kwargs...)
+    true_sol = solve(prob, Tsit5(), abstol=1e-12, reltol=1e-12)
+
+    our_u = sol.u.μ
+    true_u = true_sol.(sol.t)
+
+    @test our_u[end] ≈ true_u[end] atol=1e-6
+    @test our_u ≈ true_u atol=1e-6
+end
+
+
+@testset "EKF0 with constant steps" begin
+    @test_broken test_prob_solution_correctness(prob_ode_linear, EKF0(), steprule=:constant, dt=1e-4)
+    @test_broken test_prob_solution_correctness(prob_ode_2Dlinear, EKF0(), steprule=:constant, dt=1e-4)
+    test_prob_solution_correctness(
+        prob_ode_lotkavoltera, EKF0(), steprule=:constant, dt=1e-4)
+    test_prob_solution_correctness(
+        prob_ode_fitzhughnagumo, EKF0(), steprule=:constant, dt=1e-4)
+end
+
+
+function remake_prob_with_jac(prob)
+    prob = remake(prob, p=collect(prob.p))
+    sys = modelingtoolkitize(prob)
+    jac = eval(ModelingToolkit.generate_jacobian(sys)[2])
+    f = ODEFunction(prob.f.f, jac=jac)
+    return remake(prob, f=f)
+end
+
+@testset "EKF1 with constant steps" begin
+    test_prob_solution_correctness(
+        remake_prob_with_jac(prob_ode_lotkavoltera),
+        EKF1(), steprule=:constant, dt=1e-4)
+    test_prob_solution_correctness(
+        remake_prob_with_jac(prob_ode_fitzhughnagumo),
+        EKF1(), steprule=:constant, dt=1e-3)
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,25 +6,8 @@ using Plots
 
 @testset "ProbNumODE" begin
     @testset "Priors" begin include("priors.jl") end
-end
+    @testset "Correctness" begin include("correctness.jl") end
 
-
-@testset "Fitzhugh-Nagumo Correctness" begin
-    prob = fitzhugh_nagumo()
-    sol = solve(prob, EKF0(), steprule=:constant, dt=0.001, q=1)
-    @test length(sol) > 2
-    @test length(sol.t) == length(sol.u)
-    @test length(prob.u0) == length(sol.u[end])
-
-    true_sol = [2.010422386552278; 0.6382569402421574]  # Computed with DifferentialEquations.jl
-    @test Measurements.values.(sol[end]) ≈ true_sol atol=0.01
-end
-
-@testset "Methods" begin
-    prob = fitzhugh_nagumo()
-    sol = solve(prob, EKF0(), smooth=false)
-    sol = solve(prob, EKF1(), smooth=false)
-end
 
 @testset "Plotting" begin
     prob = fitzhugh_nagumo()
@@ -56,4 +39,6 @@ end
     #             abstol=1e-3, reltol=1e-3, q=2)
     # sol = solve(prob, EKF0(), steprule=:standard, local_errors=:filtering,
     #             abstol=1e-3, reltol=1e-3, q=2)
+end
+
 end
