d830688076b3f19985038482c3b277fab0e28122
Nathanael Bosch <nathanael.bosch@tum.de>
2020-08-20 17:21:53 +0200

    Added missing import of Plots.jl in test/solution.jl

--- test/solution.jl
+++ test/solution.jl
@@ -1,9 +1,7 @@
 using Test
-using OrdinaryDiffEq
+using Plots
 using DiffEqProblemLibrary.ODEProblemLibrary: importodeproblems; importodeproblems()
 import DiffEqProblemLibrary.ODEProblemLibrary: prob_ode_linear, prob_ode_2Dlinear, prob_ode_lotkavoltera, prob_ode_fitzhughnagumo
-using Measurements
-using ModelingToolkit
 
 import ProbNumODE: plot_stepsizes, plot_sigmas, plot_residuals, plot_errors, plot_calibration
 
