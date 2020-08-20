e8b6277b81d1f4e41901c60329cb87465b619e69
Nathanael Bosch <nathanael.bosch@tum.de>
2020-08-20 17:21:32 +0200

    Removed the error estimate tests in favor of better "correctness.jl"

--- test/correctness.jl
+++ test/correctness.jl
@@ -1,51 +1,88 @@
-# Make sure that our solvers with small steps return correct results
-# Compare with DifferentialEquations.jl.
+# Goal: Make sure that our solvers are "correct" for small steps or tolerances
+# Verify this for many (ideally all) combinations
+# Compare with an algorithm from OrdinaryDiffEq.jl with high precision
 using Test
 using OrdinaryDiffEq
+using LinearAlgebra
 using DiffEqProblemLibrary.ODEProblemLibrary: importodeproblems; importodeproblems()
 import DiffEqProblemLibrary.ODEProblemLibrary: prob_ode_linear, prob_ode_2Dlinear, prob_ode_lotkavoltera, prob_ode_fitzhughnagumo
 using ModelingToolkit
 
+import ProbNumODE: remake_prob_with_jac
 
 
-function test_prob_solution_correctness(prob, atol=1e-6, args...; kwargs...)
+@testset "Constant steps" begin
+    prob = prob_ode_lotkavoltera
+    prob = remake_prob_with_jac(prob)
 
-    prob = remake(prob, p=collect(prob.p))
+    t_eval = prob.tspan[1]:0.01:prob.tspan[end]
+    sol = solve(prob, Tsit5(), abstol=1e-12, reltol=1e-12)
+    true_sol = sol.(t_eval)
 
-    sol = solve(prob, args...; kwargs...)
-    true_sol = solve(prob, Tsit5(), abstol=1e-12, reltol=1e-12)
 
-    our_u = sol.u.μ
-    true_u = true_sol.(sol.t)
+    # abstol, reltol = 1e-3, 1e-3
+    for method in (EKF0(), EKF1()),
+        sigma in (:fixedMLE, :schober),
+        error in (:schober, :prediction, :filtering),
+        q in 1:3
 
-    @test our_u[end] ≈ true_u[end] atol=atol
-    @test our_u ≈ true_u atol=atol
+        sol = solve(prob, method, q=q,
+                    steprule=:constant, dt=1e-4,
+                    sigmarule=sigma,
+                    local_errors=error,
+                    smooth=false,
+                    )
+        diffs = [u.μ for u in sol.(t_eval)] .- true_sol
+        maxdiff = maximum(abs.(ProbNumODE.stack(diffs)))
+        mse = sum(norm.(diffs, 2)) / length(t_eval)
+
+
+        @test mse < 1e-8
+        @test maxdiff < 1e-7
+
+        # @info "Acc" method sigma error q mse maxdiff
+
+        if mse > 1e-8 || maxdiff > 1e-7
+            @info "Acc" method sigma error q mse maxdiff
+        end
+    end
 end
 
 
-@testset "EKF0 with constant steps" begin
-    @test_broken test_prob_solution_correctness(prob_ode_linear, EKF0(), steprule=:constant, dt=1e-4)
-    @test_broken test_prob_solution_correctness(prob_ode_2Dlinear, EKF0(), steprule=:constant, dt=1e-4)
-    test_prob_solution_correctness(
-        prob_ode_lotkavoltera, EKF0(), steprule=:constant, dt=1e-4)
-    test_prob_solution_correctness(
-        prob_ode_fitzhughnagumo, EKF0(), steprule=:constant, dt=1e-4)
-end
+
+@testset "Standard adaptive steps" begin
+    prob = prob_ode_lotkavoltera
+    prob = remake_prob_with_jac(prob)
+
+    t_eval = prob.tspan[1]:0.01:prob.tspan[end]
+    sol = solve(prob, Tsit5(), abstol=1e-12, reltol=1e-12)
+    true_sol = sol.(t_eval)
 
 
-function remake_prob_with_jac(prob)
-    prob = remake(prob, p=collect(prob.p))
-    sys = modelingtoolkitize(prob)
-    jac = eval(ModelingToolkit.generate_jacobian(sys)[isinplace(prob) ? 2 : 1])
-    f = ODEFunction(prob.f.f, jac=jac)
-    return remake(prob, f=f)
-end
+    # abstol, reltol = 1e-3, 1e-3
+    for method in (EKF0(), EKF1()),
+        sigma in (:fixedMLE, :schober),
+        error in (:schober, :prediction, :filtering),
+        q in 1:3
 
-@testset "EKF1 with constant steps" begin
-    test_prob_solution_correctness(
-        remake_prob_with_jac(prob_ode_lotkavoltera),
-        EKF1(), steprule=:constant, dt=1e-4)
-    test_prob_solution_correctness(
-        remake_prob_with_jac(prob_ode_fitzhughnagumo),
-        EKF1(), steprule=:constant, dt=1e-3)
+        sol = solve(prob, method, q=q,
+                    steprule=:standard, abstol=1e-9, reltol=1e-9,
+                    sigmarule=sigma,
+                    local_errors=error,
+                    smooth=false,
+                    )
+        diffs = [u.μ for u in sol.(t_eval)] .- true_sol
+        maxdiff = maximum(abs.(ProbNumODE.stack(diffs)))
+        mse = sum(norm.(diffs, 2)) / length(t_eval)
+
+
+        @test mse < 1e-5
+        @test maxdiff < 1e-5
+
+        # @info "Acc" method sigma error q mse maxdiff
+
+        if mse > 1e-5 || maxdiff > 1e-5
+            @info "Acc" method sigma error q mse maxdiff
+        end
+    end
 end
