209459065aaca0ec1d47f63b3f5458d841a8cf14
Nathanael Bosch <nathanael.bosch@tum.de>
2020-08-20 11:48:44 +0200

    Improved and added tests for the preconditioning

--- test/preconditioning.jl
+++ test/preconditioning.jl
@@ -10,9 +10,9 @@ prob = ProbNumODE.remake_prob_with_jac(prob)
 
 
 @testset "Same Solution, different states" begin
-    sol1 = solve(prob, EKF0(), q=2, steprule=:constant, dt=0.01, smooth=false,
+    sol1 = solve(prob, EKF0(), q=2, steprule=:constant, dt=0.1, smooth=false,
                  precond_dt=1.0)
-    sol2 = solve(prob, EKF0(), q=2, steprule=:constant, dt=0.01, smooth=false)
+    sol2 = solve(prob, EKF0(), q=2, steprule=:constant, dt=0.1, smooth=false)
 
     # Same solutions
     @test sol1.t ≈ sol2.t
@@ -26,4 +26,29 @@ prob = ProbNumODE.remake_prob_with_jac(prob)
     PI = sol2.solver.constants.InvPrecond
     @test sol1.x.μ ≈ [PI * x.μ for x in sol2.x]
     @test sol1.x.Σ ≈ [PI * x.Σ * PI' for x in sol2.x]
+end
+
+
+@testset "Condition numbers of A,Q" begin
+    h = 0.1*rand()
+    σ = rand()
+
+    d, q = 2, 3
+
+    A!, Q! = ProbNumODE.ibm(d, q; precond_dt=1.0)
+    Ah = diagm(0 => ones(d*(q+1)))
+    Qh = zeros(d*(q+1), d*(q+1))
+    A!(Ah, h)
+    Q!(Qh, h, σ^2)
+
+    A!_p, Q!_p = ProbNumODE.ibm(d, q; precond_dt=h)
+    Ah_p = diagm(0 => ones(d*(q+1)))
+    Qh_p = zeros(d*(q+1), d*(q+1))
+    A!_p(Ah_p, h)
+    Q!_p(Qh_p, h, σ^2)
+
+    @info "Condition numbers" cond(Qh) cond(Qh_p) cond(Ah) cond(Ah_p)
+
+    @test cond(Qh) > cond(Qh_p)
+    @test cond(Qh) > cond(Qh_p)^2
 end
