bc19a4c4355aa1e65c7af30c9d32f061929bbda9
Nathanael Bosch <nathanael.bosch@tum.de>
2020-08-21 09:13:40 +0200

    Remove include of old test file that does not exist anymore

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,6 @@ using Test
     @testset "Correctness" begin include("correctness.jl") end
     @testset "Solution" begin include("solution.jl") end
     @testset "Sigmas" begin include("sigmas.jl") end
-    @testset "Error Estimates" begin include("error_estimates.jl") end
     @testset "State Initialization" begin include("state_init.jl") end
     @testset "Preconditioning" begin include("preconditioning.jl") end
     @testset "Step Control" begin include("step_controller.jl") end
