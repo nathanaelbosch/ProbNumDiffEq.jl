90b64ebae7ac8578eabc0523d5e53907c816eef7
Nathanael Bosch <nathanael.bosch@tum.de>
2020-08-21 10:03:32 +0200

    Change to tests such that the console does something, otherwise travis cancels them

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,10 @@
 using Test
 
 
-@testset "ProbNumODE" begin
-    @testset "Priors" begin include("priors.jl") end
-    @testset "Correctness" begin include("correctness.jl") end
-    @testset "Solution" begin include("solution.jl") end
-    @testset "Sigmas" begin include("sigmas.jl") end
-    @testset "State Initialization" begin include("state_init.jl") end
-    @testset "Preconditioning" begin include("preconditioning.jl") end
-    @testset "Step Control" begin include("step_controller.jl") end
-end
+@testset "Priors" begin include("priors.jl") end
+@testset "Correctness" begin include("correctness.jl") end
+@testset "Solution" begin include("solution.jl") end
+@testset "Sigmas" begin include("sigmas.jl") end
+@testset "State Initialization" begin include("state_init.jl") end
+@testset "Preconditioning" begin include("preconditioning.jl") end
+@testset "Step Control" begin include("step_controller.jl") end
