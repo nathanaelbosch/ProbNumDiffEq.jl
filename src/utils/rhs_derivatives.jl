d551223d0826dc966469e78663add1c930a1c936
Nathanael Bosch <nathanael.bosch@tum.de>
2020-08-26 12:23:56 +0200

    Even more stable `remake_prob_with_jac`: raises error if it fails now

--- src/utils/rhs_derivatives.jl
+++ src/utils/rhs_derivatives.jl
@@ -80,11 +80,15 @@ end
 
 
 function remake_prob_with_jac(prob)
-    p = prob.p isa DiffEqBase.NullParameters ? [] : collect(prob.p)
-    prob = remake(prob, p=p)
-    sys = modelingtoolkitize(prob)
-    jac = eval(ModelingToolkit.generate_jacobian(sys)[2])
-    IIP = isinplace(prob.f)
-    f = ODEFunction{IIP}(prob.f.f, jac=jac)
-    return remake(prob, f=f)
+    try
+        p = prob.p isa DiffEqBase.NullParameters ? [] : collect(prob.p)
+        prob = remake(prob, p=p)
+        sys = modelingtoolkitize(prob)
+        jac = eval(ModelingToolkit.generate_jacobian(sys)[2])
+        IIP = isinplace(prob.f)
+        f = ODEFunction{IIP}(prob.f.f, jac=jac)
+        return remake(prob, f=f)
+    catch
+        error("Could not generate a jacobian for the problem")
+    end
 end
