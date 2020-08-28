312096170581010b29108154d2e5db778fe2d227
Nathanael Bosch <nathanael.bosch@tum.de>
2020-08-28 14:11:45 +0200

    Removed old unused preconditioning stuff

--- src/preconditioning.jl
+++ src/preconditioning.jl
@@ -6,20 +6,3 @@ function preconditioner(expected_stepsize, d, q)
     P_inv = Diagonal(kron(Diagonal(1 ./ diags), I_d))
     return (P=P, P_inv=P_inv)
 end
-function apply_preconditioner!(p, x::Gaussian)
-    x.μ .= p.P * x.μ
-    x.Σ .= p.P * x.Σ * p.P'
-end
-function undo_preconditioner!(p, x::Gaussian)
-    x.μ .= p.P_inv * x.μ
-    x.Σ .= p.P_inv * x.Σ * p.P_inv'
-end
-function undo_preconditioner!(sol, proposals, integ)
-    for s in sol
-        undo_preconditioner!(integ.preconditioner, s.x)
-    end
-    for p in proposals
-        undo_preconditioner!(integ.preconditioner, p.prediction)
-        undo_preconditioner!(integ.preconditioner, p.filter_estimate)
-    end
-end
