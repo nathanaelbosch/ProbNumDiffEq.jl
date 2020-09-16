34272ff5b95dd248e2007a45c96661d9fca2016c
Nathanael Bosch <nathanael.bosch@tum.de>
2020-09-16 20:41:22 +0200

    Improved the "correctness.jl" tests: More problems, more sigmas, better test

--- test/correctness.jl
+++ test/correctness.jl
@@ -12,61 +12,64 @@ using ModelingToolkit
 import ProbNumODE: remake_prob_with_jac
 
 
-@testset "Constant steps" begin
-    prob = prob_ode_lotkavoltera
-    prob = remake_prob_with_jac(prob)
+for (prob, probname) in [
+    (remake_prob_with_jac(prob_ode_lotkavoltera), "lotkavoltera"),
+    (remake_prob_with_jac(prob_ode_fitzhughnagumo), "fitzhughnagumo"),
+]
+    @testset "Constant steps: $probname" begin
 
-    true_sol = solve(prob, Tsit5(), abstol=1e-15, reltol=1e-15)
+        true_sol = solve(prob, Tsit5(), abstol=1e-15, reltol=1e-15)
 
-    for method in (EKF0(), EKF1()),
-        sigma in [:fixedMLE, :schober,
-                  # :fixedMAP, :fixedWeightedMLE
-                  ],
-        error in (:schober, :prediction, :filtering),
-        q in 1:3
+        for method in (EKF0(), EKF1()),
+            sigma in [:fixedMLE, :schober, :EM, :fixedWeightedMLE, :fixedMAP],
+            error in (:schober, :prediction, :filtering),
+            q in 1:3
 
-        sol = solve(prob, method, q=q,
-                    steprule=:constant, dt=1e-4,
-                    sigmarule=sigma,
-                    local_errors=error,
-                    smooth=false,
-                    )
-        diffs = sol.u .- true_sol.(sol.t)
-        maxdiff = maximum(abs.(ProbNumODE.stack(diffs)))
-        mse = sum(norm.(diffs, 2)) / length(sol.t)
+            @debug "Testing for correctness: Constant steps" probname method sigma error q
 
-        @test mse < 1e-7
-        @test maxdiff < 1e-7
+            sol = solve(prob, method, q=q,
+                        steprule=:constant, dt=1e-4,
+                        sigmarule=sigma,
+                        local_errors=error,
+                        smooth=false,
+                        )
+            @test sol.u ≈ true_sol.(sol.t)
+        end
     end
 end
 
 
 
-@testset "Standard adaptive steps" begin
-    prob = prob_ode_lotkavoltera
-    prob = remake_prob_with_jac(prob)
+for (prob, probname) in [
+    (remake_prob_with_jac(prob_ode_lotkavoltera), "lotkavoltera"),
+    (remake_prob_with_jac(prob_ode_fitzhughnagumo), "fitzhughnagumo"),
+]
+    @testset "Adaptive steps: $probname" begin
 
-    t_eval = prob.tspan[1]:0.01:prob.tspan[end]
-    sol = solve(prob, Tsit5(), abstol=1e-12, reltol=1e-12)
-    true_sol = sol.(t_eval)
+        t_eval = prob.tspan[1]:0.01:prob.tspan[end]
+        sol = solve(prob, Tsit5(), abstol=1e-12, reltol=1e-12)
+        true_sol = sol.(t_eval)
 
-    for method in (EKF0(), EKF1()),
-        # sigma in (:fixedMLE, :schober),
-        sigma in [:schober, :fixedMLE],
-        error in (:schober, :prediction, :filtering),
-        q in 1:3
+        for method in (EKF0(), EKF1()),
+            sigma in [:schober, :fixedMLE, :EM, :fixedWeightedMLE, :fixedMAP],
+            error in (:schober, :prediction, :filtering),
+            q in 1:3
 
-        sol = solve(prob, method, q=q,
-                    steprule=:standard, abstol=1e-9, reltol=1e-9,
-                    sigmarule=sigma,
-                    local_errors=error,
-                    smooth=false,
-                    )
-        diffs = sol.(t_eval) .- true_sol
-        maxdiff = maximum(abs.(ProbNumODE.stack(diffs)))
-        mse = sum(norm.(diffs, 2)) / length(t_eval)
+            @debug "Testing for correctness: Adaptive steps" probname method sigma error q
 
-        @test mse < 1e-5
-        @test maxdiff < 1e-5
+            sol = solve(prob, method, q=q,
+                        steprule=:standard, abstol=1e-9, reltol=1e-9,
+                        sigmarule=sigma,
+                        local_errors=error,
+                        smooth=false,
+                        )
+            diffs = sol.(t_eval) .- true_sol
+            maxdiff = maximum(abs.(ProbNumODE.stack(diffs)))
+            mse = sum(norm.(diffs, 2)) / length(t_eval)
+
+            @test mse < 1e-5
+            @test maxdiff < 1e-5
+            # @test sol.u ≈ true_sol.(sol.t)
+        end
     end
 end
