c9136c04bc069203823b68cdae72f3e87dd6fee8
Nathanael Bosch <nathanael.bosch@tum.de>
2020-09-25 16:41:53 +0200

    Actually that warning is too verbose

--- src/integrator_utils.jl
+++ src/integrator_utils.jl
@@ -21,7 +21,7 @@ function fix_dt_at_bounds!(integ)
     next_t = integ.t + integ.dt
     if next_t + integ.opts.dtmin^(1/2) > integ.prob.tspan[2]
         # Avoid having to make a step smaller than dtmin in the next step
-        @warn "Increasing the step size now to avoid having to do a really small step next to hit t_max"
+        @debug "Increasing the step size now to avoid having to do a really small step next to hit t_max"
         integ.dt = integ.prob.tspan[2] - integ.t
     end
 end
