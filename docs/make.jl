afd101cb510e04bf15892a31aea41bc8ce628255
Nathanael Bosch <nathanael.bosch@tum.de>
2020-10-28 19:03:35 +0100

    explicitly set `devbranch="main"`

--- docs/make.jl
+++ docs/make.jl
@@ -18,5 +18,6 @@ makedocs(
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo = "github.com/nathanaelbosch/ODEFilters.jl.git"
+    repo = "github.com/nathanaelbosch/ODEFilters.jl.git",
+    devbranch="main"
 )
