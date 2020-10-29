325dafa0cc6988ccb1ef4c2658647ab1288dfedf
Nathanael Bosch <nathanael.bosch@gmail.com>
2020-10-29 12:35:44 +0100

    Initialize with TaylorSeries.jl instead of ForwardDiff.jl!

--- src/state_initialization.jl
+++ src/state_initialization.jl
@@ -15,7 +15,8 @@ function initialize_without_derivatives(u0, f, p, t0, order, var=1e-3)
 end
 
 
-function initialize_with_derivatives(u0, f, p, t0, order::Int)
+function initialize_with_derivatives_forwarddiff(u0, f, p, t0, order::Int)
+    @warn "Better to use the TaylorSeries.jl approach"
     f = isinplace(f) ? iip_to_oop(f) : f
 
     d = length(u0)
@@ -36,6 +37,43 @@ function initialize_with_derivatives(u0, f, p, t0, order::Int)
         df(u, p, t) = dfdu(u, p, t) * f(u, p, t) + dfdt(u, p, t)
         push!(f_derivatives, df)
         m0[o*d+1:(o+1)*d] = df(u0, p, t0)
+    end
+
+    return m0, P0
+end
+
+
+function initialize_with_derivatives(u0, f, p, t0, order::Int)
+    taylor_order = 15
+    @assert taylor_order > order "The internal order of TaylorSeries.jl is smaller than the requested order of derivatives"
+
+    f = isinplace(f) ? iip_to_oop(f) : f
+
+    d = length(u0)
+    q = order
+
+    uElType = eltype(u0)
+    m0 = zeros(uElType, d*(q+1))
+    P0 = zeros(uElType, d*(q+1), d*(q+1))
+
+    m0[1:d] .= u0
+    m0[d+1:2d] .= f(u0, p, t0)
+
+    # Make sure that the vector field f does not depend on t
+    f_t_taylor = taylor_expand(t -> f(u0, p, t), t0)
+    @assert !(eltype(f_t_taylor) <: TaylorN)
+
+    fp = taylor_expand(u -> f(u, p, t0), u0; order=taylor_order)
+    f_derivatives = [fp]
+    for o in 2:q
+        _curr_f_deriv = f_derivatives[end]
+        # dfdu(u, p, t) = ForwardDiff.jacobian(u -> _curr_f_deriv(u, p, t), u)
+        dfdu = [derivative.(_curr_f_deriv, 1) derivative.(_curr_f_deriv, 2)]
+        # dfdt(u, p, t) = ForwardDiff.derivative(t -> _curr_f_deriv(u, p, t), t)
+        # df(u, p, t) = dfdu(u, p, t) * f(u, p, t) + dfdt(u, p, t)
+        df = dfdu * fp
+        push!(f_derivatives, df)
+        m0[o*d+1:(o+1)*d] = evaluate(df)
     end
 
     return m0, P0
