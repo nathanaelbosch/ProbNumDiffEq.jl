d55d92e859113f34940c6ddf3bf8bcb5cc4ee64c
Nathanael Bosch <nathanael.bosch@gmail.com>
2020-12-02 16:02:41 +0100

    More efficient `apply_diffusion`

--- src/ODEFilters.jl
+++ src/ODEFilters.jl
@@ -22,7 +22,8 @@ using RecursiveArrayTools
 @reexport using PSDMatrices
 import PSDMatrices: X_A_Xt
 X_A_Xt(A, X) = X*A*X'
-apply_diffusion(Q, diffmat) = X_A_Xt(Q, sqrt.(diffmat))
+apply_diffusion(Q, diffmat::Diagonal) = X_A_Xt(Q, sqrt.(diffmat))
+apply_diffusion(Q, diffmat::Number) = Q*diffmat
 
 
 # All the Gaussian things
