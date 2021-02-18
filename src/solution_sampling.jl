9e3a71a98c020ec38ccf109db2e0f6dcc9317a25
Nathanael Bosch <nathanael.bosch@gmail.com>
2021-02-18 17:04:19 +0100

    Fixed the remaining failing sampling test

--- src/solution_sampling.jl
+++ src/solution_sampling.jl
@@ -12,39 +12,22 @@ function _rand(x::Gaussian, n::Int=1)
 end
 
 
-function sample_back(x_curr::SRGaussian, x_next_sample::AbstractVector, Ah::AbstractMatrix, Qh::SRMatrix, PI=I)
-    m_p, P_p = Ah*x_curr.μ, Ah*x_curr.Σ*Ah' + Qh
-    P_p_inv = inv(Symmetric(P_p))
-    Gain = x_curr.Σ * Ah' * P_p_inv
-
-    m = x_curr.μ + Gain * (x_next_sample - m_p)
-
-    # P = X_A_Xt(x_curr.Σ, (I - Gain*Ah)) + X_A_Xt(Qh, Gain)
-    _R = [x_curr.Σ.squareroot' * (I - Gain*Ah)'
-          Qh.squareroot' * Gain']
-    _, P_L = qr(_R)
-    P = SRMatrix(P_L)
-
-    assert_nonnegative_diagonal(P)
-    return Gaussian(m, P)
-end
-
-
 function sample(sol::ProbODESolution, n::Int=1)
     @assert sol.interp.smooth "sampling not implemented for non-smoothed posteriors"
     sample(sol.t, sol.x_filt, sol.diffusions, sol.t, sol.interp, n)
 end
 function sample(ts, xs, diffusions, difftimes, posterior, n::Int=1)
+    @assert length(diffusions)+1 == length(difftimes)
 
     @unpack A, Q, d, q, Precond = posterior
     E0 = kron([i==1 ? 1 : 0 for i in 1:q+1]', diagm(0 => ones(d)))
-    dim = d*(q+1)
+    D = d*(q+1)
 
     x = xs[end]
     sample = _rand(x, n)
-    @assert size(sample) == (dim, n)
+    @assert size(sample) == (D, n)
 
-    sample_path = zeros(length(ts), dim, n)
+    sample_path = zeros(length(ts), D, n)
     sample_path[end, :, :] .= sample
     # @info "final value and samples" x.μ sample sample_path[end, :]
 
@@ -62,7 +45,8 @@ function sample(ts, xs, diffusions, difftimes, posterior, n::Int=1)
             sample_p = P*sample_path[i+1, :, j]
             x_prev_p = P*xs[i]
 
-            prev_sample_p = sample_back(x_prev_p, sample_p, A, Qh, PI)
+            prev_sample_p, _ = smooth(
+                x_prev_p, Gaussian(sample_p, SRMatrix(zeros(D,D))), A, Qh)
 
             # sample_path[i, :, j] .= PI*prev_sample_p.μ
             sample_path[i, :, j] .= PI*_rand(prev_sample_p)[:]
