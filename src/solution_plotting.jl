2733be2348db3c8a29eca443c34485b8ebc27c16
Nathanael Bosch <nathanael.bosch@gmail.com>
2021-03-09 20:53:43 +0100

    Added support for the `denseplot` plotting argument

--- src/solution_plotting.jl
+++ src/solution_plotting.jl
@@ -1,14 +1,22 @@
 ########################################################################################
 # Plotting
 ########################################################################################
-@recipe function f(sol::AbstractProbODESolution; ribbon_width=1.96)
-    times = range(sol.t[1], sol.t[end], length=1000)
-    dense_post = sol(times).u
-    values = stack(mean(dense_post))
-    stds = stack(std(dense_post))
-    ribbon --> ribbon_width * stds
+@recipe function f(sol::AbstractProbODESolution;
+                   denseplot = sol.dense,
+                   plotdensity = 1000,
+                   ribbon_width=1.96)
     xguide --> "t"
     yguide --> "u(t)"
     label --> hcat(["u$(i)(t)" for i in 1:length(sol.u[1])]...)
-    return times, values
+    if denseplot
+        times = range(sol.t[1], sol.t[end], length=plotdensity)
+        dense_post = sol(times).u
+        values = stack(mean(dense_post))
+        stds = stack(std(dense_post))
+        ribbon --> ribbon_width * stds
+        return times, values
+    else
+        ribbon --> ribbon_width * stack(std.(sol.pu))
+        return sol.t, stack(sol.u)
+    end
 end
