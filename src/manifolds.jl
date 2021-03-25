e0587c9ff89a37279b1f1c51e579bcb6faed5049
Nathanael Bosch <nathanael.bosch@gmail.com>
2021-03-25 13:12:09 +0100

    More efficiency by using DiffResults

--- src/manifolds.jl
+++ src/manifolds.jl
@@ -1,8 +1,12 @@
 function manifold_update!(x, h, maxiters=1, check=false)
     H = ForwardDiff.gradient(h, x.μ)
+    result = DiffResults.GradientResult(x.μ)
+    result = ForwardDiff.gradient!(result, h, x.μ)
+    z = DiffResults.value(result)
+    H = DiffResults.gradient(result)
     @assert H isa AbstractVector
     _H = reshape(H, 1, length(H))
-    meas = Gaussian([h(x.μ)], X_A_Xt(x.Σ, _H))
+    meas = Gaussian([z], X_A_Xt(x.Σ, _H))
 
     if iszero(meas.μ) return end
     update!(x, x, meas, _H)
