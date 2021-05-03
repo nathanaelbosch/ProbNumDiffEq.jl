58219e17020447adda8a7988858863c7c1345907
Nathanael Bosch <nathanael.bosch@gmail.com>
2021-05-03 21:39:13 +0200

    Changed the IEKF to use vector-valued functions and Jacobians now

--- src/filtering.jl
+++ src/filtering.jl
@@ -126,14 +126,17 @@ function iekf_update(x::Gaussian, h::Function, z=0, R=0)
     i=0
     μ_i = copy(x.μ)
     local K_i, S_i, H_i, h_i
-    result = DiffResults.GradientResult(μ_i)
+    # result = DiffResults.GradientResult(μ_i)
     for i in 1:100
 
         # Measure
-        result = ForwardDiff.gradient!(result, h, μ_i)
-        h_i = DiffResults.value(result)
-        H_i = DiffResults.gradient(result)
-        H_i = reshape(H_i, (1, length(H_i)))
+        # result = ForwardDiff.gradient!(result, h, μ_i)
+        # h_i = DiffResults.value(result)
+        # H_i = DiffResults.gradient(result)
+        # H_i = reshape(H_i, (1, length(H_i)))
+        h_i = h(μ_i)
+        H_i = ForwardDiff.jacobian(h, μ_i)
+        # H_i = reshape(H_i, (1, length(H_i)))
 
         S_i_L = H_i*x.Σ.squareroot
         S_i = S_i_L * S_i_L'
@@ -143,7 +146,7 @@ function iekf_update(x::Gaussian, h::Function, z=0, R=0)
         # @info "iekf" i h_i norm(μ_i_new .- μ_i) μ_i μ_i_new
         # μ_i μ_i_new
 
-        if norm(μ_i_new .- μ_i) < ϵ₁ && abs(h_i) < ϵ₂
+        if norm(μ_i_new .- μ_i) < ϵ₁ && norm(h_i) < ϵ₂
             μ_i = μ_i_new
             break
         end
