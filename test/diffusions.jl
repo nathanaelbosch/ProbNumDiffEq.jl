9d4aa043581d202f4c0c6554cd1dabd8240713e1
Nathanael Bosch <nathanael.bosch@gmail.com>
2021-08-12 17:03:30 +0200

    Made the diffusion tests cheaper to run

--- test/diffusions.jl
+++ test/diffusions.jl
@@ -10,28 +10,33 @@ import DiffEqProblemLibrary.ODEProblemLibrary: prob_ode_linear, prob_ode_2Dlinea
     true_sol = solve(prob, Tsit5(), abstol=1e-12, reltol=1e-12)
 
     @testset "Time-Varying Diffusion" begin
-        sol = solve(prob, EK0(diffusionmodel=:dynamic), adaptive=false, dt=1e-4)
-        @test sol.u ≈ true_sol.(sol.t)
+        sol = solve(prob, EK0(diffusionmodel=:dynamic, smooth=false),
+                    dense=false, adaptive=false, dt=1e-4)
+        @test sol.u[end] ≈ true_sol.(sol.t)[end]
     end
 
     @testset "Time-Fixed Diffusion" begin
-        sol = solve(prob, EK0(diffusionmodel=:fixed), adaptive=false, dt=1e-4)
-        @test sol.u ≈ true_sol.(sol.t)
+        sol = solve(prob, EK0(diffusionmodel=:fixed, smooth=false),
+                    dense=false, adaptive=false, dt=1e-4)
+        @test sol.u[end] ≈ true_sol.(sol.t)[end]
     end
 
     @testset "Time-Varying Diagonal Diffusion" begin
-        sol = solve(prob, EK0(diffusionmodel=:dynamicMV), adaptive=false, dt=1e-4)
-        @test sol.u ≈ true_sol.(sol.t)
+        sol = solve(prob, EK0(diffusionmodel=:dynamicMV, smooth=false),
+                    dense=false, adaptive=false, dt=1e-4)
+        @test sol.u[end] ≈ true_sol.(sol.t)[end]
     end
 
     @testset "Time-Fixed Diagonal Diffusion" begin
-        sol = solve(prob, EK0(diffusionmodel=:fixedMV), adaptive=false, dt=1e-4)
-        @test sol.u ≈ true_sol.(sol.t)
+        sol = solve(prob, EK0(diffusionmodel=:fixedMV, smooth=false),
+                    dense=false, adaptive=false, dt=1e-4)
+        @test sol.u[end] ≈ true_sol.(sol.t)[end]
     end
 
     @testset "Time-Fixed Diffusion MAP" begin
-        sol = solve(prob, EK0(diffusionmodel=:fixedMAP), adaptive=false, dt=1e-4)
-        @test sol.u ≈ true_sol.(sol.t)
+        sol = solve(prob, EK0(diffusionmodel=:fixedMAP, smooth=false),
+                    dense=false, adaptive=false, dt=1e-4)
+        @test sol.u[end] ≈ true_sol.(sol.t)[end]
     end
 
 end
