73fb0c99bb8a406f6e8fe53261c7adc133e61a39
Nathanael Bosch <nathanael.bosch@gmail.com>
2021-08-23 19:32:12 +0200

    Better, more thorough convergence tests

--- test/convergence.jl
+++ test/convergence.jl
@@ -3,9 +3,6 @@ using ProbNumDiffEq
 using DiffEqDevTools
 
 
-TESTTOL = 0.2
-
-
 # Simple linear problem
 prob = ODEProblem(
     ODEFunction(
@@ -14,21 +11,35 @@ prob = ODEProblem(
         analytic=(u0, p, t) -> u0 .* exp(1.01*t),
     ), [big(1/2)], big.((0.0, 1.0)))
 
+# Step-sizes
+dts1 = 1 .//2 .^(7:-1:4)
+dts2 = 1 .//2 .^(10:-1:8)
+
 
 @testset "EK0(order=$q) convergence" for q in 1:5
-    dts = 1 .//2 .^(8:-1:4)
-    sim = test_convergence(dts, prob, EK0(order=q))
-    @test sim.𝒪est[:final] ≈ q+1 atol=0.2
-    @test sim.𝒪est[:l2] ≈ q+1 atol=0.25
-    @test sim.𝒪est[:l∞] ≈ q+1 atol=0.2
+    sim = test_convergence(dts1, prob, EK0(order=q))
+    @test sim.𝒪est[:final] ≈ q+1 atol=0.3
+    @test sim.𝒪est[:l2] ≈ q+1 atol=0.3
+    @test sim.𝒪est[:l∞] ≈ q+1 atol=0.3
+end
+@testset "EK0(order=$q) convergence" for q in [7, 8]
+    sim = test_convergence(dts2, prob, EK0(order=q))
+    @test sim.𝒪est[:final] ≈ q+1 atol=1.5
+    @test sim.𝒪est[:l2] ≈ q+1 atol=1
+    @test sim.𝒪est[:l∞] ≈ q+1 atol=1
 end
 
 
 
 @testset "EK1(order=$q) convergence" for q in 1:5
-    dts = 1 .//2 .^(8:-1:4)
-    sim = test_convergence(dts, prob, EK1(order=q))
+    sim = test_convergence(dts1, prob, EK1(order=q))
     @test sim.𝒪est[:final] ≈ q+1 atol=0.55
     @test sim.𝒪est[:l2] ≈ q+1 atol=0.5
     @test sim.𝒪est[:l∞] ≈ q+1 atol=0.5
+end
+@testset "EK1(order=$q) convergence" for q in [7, 8, 10]
+    sim = test_convergence(dts2, prob, EK1(order=q))
+    @test sim.𝒪est[:final] ≈ q+1 atol=1
+    @test sim.𝒪est[:l2] ≈ q+1 atol=1
+    @test sim.𝒪est[:l∞] ≈ q+1 atol=1
 end
