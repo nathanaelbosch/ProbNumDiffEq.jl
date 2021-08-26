05281fc74f2584c4ffa38c078bf710dae4bf1c08
Nathanael Bosch <nathanael.bosch@gmail.com>
2021-08-26 12:14:53 +0200

    Provide a default for the projection elType

--- src/projection.jl
+++ src/projection.jl
@@ -1,4 +1,4 @@
-function projection(d, q, elType)
+function projection(d, q, elType=typeof(1.0))
     Proj(deriv) =
         # deriv > q ? error("Projection called for non-modeled derivative") :
         kron([i==(deriv+1) ? 1 : 0 for i in 1:q+1]', diagm(0 => ones(elType, d)))
