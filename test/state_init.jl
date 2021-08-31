5ef214e8e0958f70df9bfc7334b71319e3dd1d7a
Nathanael Bosch <nathanael.bosch@gmail.com>
2021-08-31 11:44:34 +0200

    Better test to check on the initialization

--- test/state_init.jl
+++ test/state_init.jl
@@ -4,7 +4,7 @@ using LinearAlgebra
 using Test
 
 using DiffEqProblemLibrary.ODEProblemLibrary: importodeproblems; importodeproblems()
-import DiffEqProblemLibrary.ODEProblemLibrary: prob_ode_pleiades
+import DiffEqProblemLibrary.ODEProblemLibrary: prob_ode_fitzhughnagumo, prob_ode_pleiades
 
 
 d = 2
@@ -45,6 +45,46 @@ end
     dfs = ProbNumDiffEq.taylormode_get_derivatives(prob.u0, prob.f, prob.p, prob.tspan[1], q)
     @test length(dfs) == q+1
     @test true_init_states ≈ vcat(dfs...)
+end
+
+
+
+@testset "Compare TaylorModeInit and RungeKuttaInit" begin
+	  # This has not worked before with the Taylor-Mode init!
+    # The high dimensions made the runtimes explode
+
+    prob = prob_ode_fitzhughnagumo
+    d = length(prob.u0)
+
+    integ = init(prob, EK0(order=8));
+    tm_init = integ.cache.x.μ
+
+    @testset "Order $o" for o in (3, 4, 5, 6, 7)
+        integ = init(prob, EK0(order=o, initialization=RungeKuttaInit()));
+        rk_init = integ.cache.x.μ
+
+        # These are fit via the initial values + autodiff, and should be good
+        @test tm_init[1:d] ≈ rk_init[1:d]
+        @test tm_init[d+1:2d] ≈ rk_init[d+1:2d]
+        (o > 1) && @test tm_init[2d+1:3d] ≈ rk_init[2d+1:3d]
+
+        # Test if the others are correct, up to order 5
+        (5 > o > 2) && @test tm_init[3d+1:4d] ≈ rk_init[3d+1:4d] rtol=1e-2
+        (o == 5) && @test tm_init[3d+1:4d] ≈ rk_init[3d+1:4d] rtol=6e-1
+        (5 > o > 3) && @test tm_init[4d+1:5d] ≈ rk_init[4d+1:5d] rtol=1e-3
+        (o == 5) && @test tm_init[4d+1:5d] ≈ rk_init[4d+1:5d] rtol=1e-2
+        (6 > o > 4) && @test tm_init[5d+1:6d] ≈ rk_init[5d+1:6d] rtol=2e-1
+
+
+        # Test if the covariance reflects the true error
+        Cs = integ.cache.x.Σ.squareroot[3d+1:end, 3d+1:end]
+        err = (rk_init .- tm_init[1:length(rk_init)])[3d+1:end]
+        whitened_err = Cs \ err
+        _l = length(whitened_err)
+        # `whitened_err` should be standard gaussian, so let's just check that they're small
+        @test all(abs.(whitened_err) .< 2e-1)
+
+    end
 end
 
 
