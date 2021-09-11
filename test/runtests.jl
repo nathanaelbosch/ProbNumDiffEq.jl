e3f34ec7c1dd63140770d124d05687586138bc66
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2021-09-11 17:46:29 +0200

    Improve runtests.jl a bit, by moving the print's into the macro

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
-using ProbNumDiffEq
 using Test
+using ProbNumDiffEq
 using Statistics
 using UnPack
 using Plots
@@ -20,71 +20,62 @@ import DiffEqProblemLibrary.ODEProblemLibrary:
 using TimerOutputs
 const to = TimerOutput()
 macro timedtestset(name, code)
-    return esc(:(@timeit to $name @testset $name $code))
+    return esc(:(
+        println("Start testset: ", $name);
+        @timeit to $name @testset $name $code;
+        println("Done.");
+    ))
 end
 
 @testset "ProbNumDiffEq" begin
-    println("Correctness")
     @timedtestset "Correctness" begin
         include("correctness.jl")
     end
 
-    println("Filtering")
     @timedtestset "Filtering" begin
         include("filtering.jl")
     end
 
-    println("Convergence")
     @timedtestset "Convergence" begin
         include("convergence.jl")
     end
 
-    println("Priors")
     @timedtestset "Priors" begin
         include("priors.jl")
     end
 
-    println("Solution")
     @timedtestset "Solution" begin
         include("solution.jl")
     end
 
-    println("Diffusions")
     @timedtestset "Diffusions" begin
         include("diffusions.jl")
     end
 
-    println("State Initialization")
     @timedtestset "State Initialization" begin
         include("state_init.jl")
     end
 
-    println("Preconditioning")
     @timedtestset "Preconditioning" begin
         include("preconditioning.jl")
     end
 
-    println("Smoothing")
     @timedtestset "Smoothing" begin
         include("smoothing.jl")
     end
 
-    println("Errors")
     @timedtestset "Errors" begin
         include("errors.jl")
     end
 
-    println("IEKS")
     @timedtestset "IEKS" begin
         include("ieks.jl")
     end
 
-    println("Automatic Differentiation")
     @timedtestset "Automatic Differentiation" begin
         include("autodiff.jl")
     end
 
-    println("Specific Problems")
     @timedtestset "Specific Problems" begin
         include("specific_problems.jl")
     end
