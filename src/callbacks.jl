8e5818fad856a0dee7939ae1f366dba46c909966
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2021-10-03 02:07:14 +0200

    Make the ManifoldUpdate callback an IEKF update

--- src/callbacks.jl
+++ src/callbacks.jl
@@ -1,4 +1,4 @@
-function ManifoldUpdate(residualf)
+function ManifoldUpdate(residualf; maxiters=100, ϵ₁=1e-25, ϵ₂=1e-15)
     condition(u, t, integ) = true
 
     function affect!(integ)
@@ -8,12 +8,23 @@ function ManifoldUpdate(residualf)
         f(m) = residualf(SolProj * m)
 
         m, C = x
-        z = f(m)
-        J = ForwardDiff.jacobian(f, m)
-        S = X_A_Xt(C, J)
+        m_i = copy(m)
 
-        x_out = update(x, Gaussian(z, S), J)
-        copy!(x, x_out)
+        local m_i_new, C_i_new
+        for i in 1:maxiters
+            z = f(m_i)
+            J = ForwardDiff.jacobian(f, m_i)
+            S = X_A_Xt(C, J)
+
+            m_i_new, C_i_new = update(x, Gaussian(z .+ (J * (m - m_i)), S), J)
+
+            if norm(m_i_new .- m_i) < ϵ₁ && norm(z) < ϵ₂
+                break
+            end
+            m_i = m_i_new
+        end
+        copy!(x, Gaussian(m_i_new, C_i_new))
+
         return nothing
     end
     return DiscreteCallback(condition, affect!)
