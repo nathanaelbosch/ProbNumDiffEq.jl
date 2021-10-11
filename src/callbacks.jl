658652667ddebf0aa6b72db51ca6154d4f1b66e7
Nathanael Bosch <nathanael.bosch@gmail.com>
2021-10-11 10:21:32 +0200

    Improve callback code structure (#84)
    * Improve callback code structure
    
    * JuliaFormatter.jl

--- src/callbacks.jl
+++ src/callbacks.jl
@@ -1,31 +1,31 @@
+function manifoldupdate!(integ, residualf; maxiters=100, ϵ₁=1e-25, ϵ₂=1e-15)
+    @unpack x, SolProj = integ.cache
+    f(m) = residualf(SolProj * m)
+    return copy!(x, manifoldupdate(x, f; maxiters=maxiters, ϵ₁=ϵ₁, ϵ₂=ϵ₂))
+end
+
+function manifoldupdate(x, f; maxiters=100, ϵ₁=1e-25, ϵ₂=1e-15)
+    m, C = x
+    m_i = copy(m)
+
+    local m_i_new, C_i_new
+    for i in 1:maxiters
+        z = f(m_i)
+        J = ForwardDiff.jacobian(f, m_i)
+        S = X_A_Xt(C, J)
+
+        m_i_new, C_i_new = update(x, Gaussian(z .+ (J * (m - m_i)), S), J)
+
+        if norm(m_i_new .- m_i) < ϵ₁ && norm(z) < ϵ₂
+            break
+        end
+        m_i = m_i_new
+    end
+    return Gaussian(m_i_new, C_i_new)
+end
+
 function ManifoldUpdate(residualf, args...; maxiters=100, ϵ₁=1e-25, ϵ₂=1e-15, kwargs...)
     condition(u, t, integ) = true
-
-    function affect!(integ)
-        @unpack u = integ
-        @unpack x, SolProj = integ.cache
-
-        f(m) = residualf(SolProj * m)
-
-        m, C = x
-        m_i = copy(m)
-
-        local m_i_new, C_i_new
-        for i in 1:maxiters
-            z = f(m_i)
-            J = ForwardDiff.jacobian(f, m_i)
-            S = X_A_Xt(C, J)
-
-            m_i_new, C_i_new = update(x, Gaussian(z .+ (J * (m - m_i)), S), J)
-
-            if norm(m_i_new .- m_i) < ϵ₁ && norm(z) < ϵ₂
-                break
-            end
-            m_i = m_i_new
-        end
-        copy!(x, Gaussian(m_i_new, C_i_new))
-
-        return nothing
-    end
+    affect!(integ) = manifoldupdate!(integ, residualf; maxiters=maxiters, ϵ₁=ϵ₁, ϵ₂=ϵ₂)
     return DiscreteCallback(condition, affect!, args...; kwargs...)
 end
