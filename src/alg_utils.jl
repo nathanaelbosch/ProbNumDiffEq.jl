aca565dc7413e365cb0c8aa5da601086a0ed8251
Nathanael Bosch <nathanael.bosch@gmail.com>
2022-01-04 14:04:45 +0100

    Add a standardtag function to fix errors (#101)
    * Add standardtag compatibility
    
    * Now it looks better: True for EK1, false for EK0
    
    Since it looks like it has something to do with implicit algs etc
    
    * Need to cover all possible `AbstractEK`s

--- src/alg_utils.jl
+++ src/alg_utils.jl
@@ -6,6 +6,8 @@
 OrdinaryDiffEq.alg_autodiff(alg::AbstractEK) = true
 OrdinaryDiffEq.alg_autodiff(alg::EK1{CS,AD}) where {CS,AD} = AD
 OrdinaryDiffEq.alg_difftype(alg::EK1{CS,AD,DiffType}) where {CS,AD,DiffType} = DiffType
+OrdinaryDiffEq.standardtag(alg::EK1) = true
+OrdinaryDiffEq.standardtag(alg::AbstractEK) = false
 @inline DiffEqBase.get_tmp_cache(integ, alg::EK1, cache) = (cache.tmp, cache.atmp)
 OrdinaryDiffEq.get_chunksize(alg::AbstractEK) = Val(0)
 OrdinaryDiffEq.isfsal(alg::AbstractEK) = false
