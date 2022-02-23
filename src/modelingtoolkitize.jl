3f336cf7993102d25e606f7c6b5b68eed227c7bf
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-02-23 21:59:23 +0100

    Have modelingtoolkitize_with_jac return a error if f.analytic exists

--- src/modelingtoolkitize.jl
+++ src/modelingtoolkitize.jl
@@ -4,5 +4,9 @@
 Add a jacobian function to the ODE function, using ModelingToolkit.jl.
 This function is also used to internally transform out-of-place problems to in-place.
 """
-modelingtoolkitize_with_jac(prob::ODEProblem; jac=true) =
-    ODEProblem(modelingtoolkitize(prob), prob.u0, prob.tspan, jac=jac)
+function modelingtoolkitize_with_jac(prob::ODEProblem; jac=true)
+    if !isnothing(prob.f.analytic)
+        error("`modelingtoolkitize_with_jac` does not preserve analytic solutions")
+    end
+    return ODEProblem(modelingtoolkitize(prob), prob.u0, prob.tspan, jac=jac)
+end
