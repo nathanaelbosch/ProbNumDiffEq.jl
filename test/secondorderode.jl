69a645da80e36d92210985305556e74577c73ca5
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-02-24 09:58:28 +0100

    Add OOP secondorder ODEs back to the tests again

--- test/secondorderode.jl
+++ test/secondorderode.jl
@@ -1,4 +1,5 @@
 using ProbNumDiffEq
+using OrdinaryDiffEq
 using Test
 
 du0 = [0.0]
@@ -12,32 +13,34 @@ function vanderpol!(ddu, du, u, p, t)
 end
 prob_iip = SecondOrderODEProblem(vanderpol!, du0, u0, tspan, p)
 
-# function vanderpol(du, u, p, t)
-#     μ = p[1]
-#     ddu = μ .* ((1 .- u .^ 2) .* du .- u)
-#     return ddu
-# end
-# prob_oop = SecondOrderODEProblem(vanderpol, du0, u0, tspan, p)
+function vanderpol(du, u, p, t)
+    μ = p[1]
+    ddu = μ .* ((1 .- u .^ 2) .* du .- u)
+    return ddu
+end
+prob_oop = SecondOrderODEProblem(vanderpol, du0, u0, tspan, p)
 
 appxsol = solve(prob_iip, Tsit5(), abstol=1e-7, reltol=1e-7)
 
 @testset "IIP" begin
     for Alg in (EK0, EK1)
         @testset "$Alg" begin
-            @test solve(prob_iip, Alg()) isa ProbNumDiffEq.ProbODESolution
-            @test solve(prob_iip, Alg()).u[end] ≈ appxsol.u[end] rtol = 1e-3
+            sol = solve(prob_iip, Alg())
+            @test sol isa ProbNumDiffEq.ProbODESolution
+            @test sol.u[end] ≈ appxsol.u[end] rtol = 1e-3
         end
     end
 end
 
-# @testset "OOP" begin
-#     for Alg in (EK0, EK1)
-#         @testset "$Alg" begin
-#             @test solve(prob_oop, Alg()) isa ProbNumDiffEq.ProbODESolution
-#             @test solve(prob_oop, Alg()).u[end] ≈ appxsol.u[end] rtol = 1e-3
-#         end
-#     end
-# end
+@testset "OOP" begin
+    for Alg in (EK0, EK1)
+        @testset "$Alg" begin
+            sol = solve(prob_oop, Alg())
+            @test sol isa ProbNumDiffEq.ProbODESolution
+            @test sol.u[end] ≈ appxsol.u[end] rtol = 1e-3
+        end
+    end
+end
 
 @testset "ClassicSolverInit for SecondOrderODEProblems" begin
     @test_broken solve(prob_iip, EK1(initialization=ClassicSolverInit())) isa
