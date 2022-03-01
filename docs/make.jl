06aa8562638671877e007b2e2e234a7444a7be58
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-03-01 20:19:19 +0100

    Uncomment the `deploydocs` line

--- docs/make.jl
+++ docs/make.jl
@@ -15,4 +15,4 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-# deploydocs(repo="github.com/nathanaelbosch/ProbNumDiffEq.jl.git", devbranch="main")
+deploydocs(repo="github.com/nathanaelbosch/ProbNumDiffEq.jl.git", devbranch="main")
