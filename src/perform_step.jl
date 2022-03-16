8c5195da4cc51bc8dd710a05f09c0049d84b2ee0
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-03-16 11:23:39 +0100

    Remove the warning and instead raise an error
    Previously, we warned the user when they provide a DynamicalODEFunction to
    inform them that we can not handle true DynamicalODEProblems, but only
    SecondOrderODEProblems. Now, we instead check if the problem has the
    SecondOrderODEProblem trait and raise an error if not.

--- src/perform_step.jl
+++ src/perform_step.jl
@@ -1,5 +1,14 @@
 # Called in the OrdinaryDiffEQ.__init; All `OrdinaryDiffEqAlgorithm`s have one
 function OrdinaryDiffEq.initialize!(integ, cache::GaussianODEFilterCache)
+    if integ.f isa DynamicalODEFunction && !(integ.sol.prob.problem_type isa SecondOrderODEProblem)
+        error("""
+            The given problem is a `DynamicalODEProblem`, but not a \
+            `SecondOrderODEProblem`. This can not be handled by ProbNumDiffEq.jl right \
+            now. Please check if the problem can be formulated as a second order ODE. If \
+            not, please open a new github issue!
+            """)
+    end
+
     if integ.opts.dense && !integ.alg.smooth
         error("To use `dense=true` you need to set `smooth=true`!")
     elseif !integ.opts.dense && integ.alg.smooth
