8f714a8545092a69dddffab20d32f5a75492112c
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-05-10 22:06:53 +0300

    Remove some old commented out lines

--- src/solution.jl
+++ src/solution.jl
@@ -71,10 +71,7 @@ function DiffEqBase.build_solution(
     d = length(prob.u0)
     uElType = eltype(prob.u0)
     D = d
-    pu_cov =
-        # alg isa EK0 && !(prob.f isa DynamicalODEFunction) ?
-        # SRMatrix(zeros(uElType, d, D), Diagonal(zeros(uElType, d, d))) :
-        SRMatrix(zeros(uElType, d, D))
+    pu_cov = SRMatrix(zeros(uElType, d, D))
     x_cov = SRMatrix(zeros(uElType, d, d))
     pu = StructArray{Gaussian{Vector{uElType},typeof(pu_cov)}}(undef, 0)
     x_filt = StructArray{Gaussian{Vector{uElType},typeof(x_cov)}}(undef, 0)
