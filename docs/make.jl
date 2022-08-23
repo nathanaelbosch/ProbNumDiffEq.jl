d583e8cad8c63ff2b2c60cd31692779612e47d20
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-08-23 07:17:15 +0000

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -24,4 +24,8 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-deploydocs(repo="github.com/nathanaelbosch/ProbNumDiffEq.jl.git", devbranch="main")
+deploydocs(
+    repo="github.com/nathanaelbosch/ProbNumDiffEq.jl.git",
+    devbranch="main",
+    push_preview=true,
+)
