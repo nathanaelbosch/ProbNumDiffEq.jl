16ba14cb63a27ac9637e0d855d0705e3aa56b313
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-08-26 13:27:15 +0200

    Re-organize tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,73 +1,74 @@
-using Test, SafeTestsets, Aqua
+using Test, SafeTestsets, Aqua, TimerOutputs
 using ProbNumDiffEq
 using ODEProblemLibrary
-using TimerOutputs
-const to = TimerOutput()
+
+to = TimerOutput()
 macro timedtestset(name, code)
+    return esc(:(println("Start testset: ", $name);
+    @timeit to $name @testset $name $code;
+    println("Done.")))
+end
+macro timedsafetestset(name, code)
     return esc(:(println("Start testset: ", $name);
     @timeit to $name @safetestset $name $code;
     println("Done.")))
 end
 
 @testset "ProbNumDiffEq" begin
-    @timedtestset "Correctness" begin
-        include("correctness.jl")
+    @timedtestset "Core" begin
+        @timedsafetestset "Filtering" begin
+            include("filtering.jl")
+        end
+        @timedsafetestset "Priors" begin
+            include("priors.jl")
+        end
+        @timedsafetestset "Preconditioning" begin
+            include("preconditioning.jl")
+        end
     end
 
-    @timedtestset "Filtering" begin
-        include("filtering.jl")
+    @timedtestset "Solver Correctness" begin
+        @timedsafetestset "Correctness" begin
+            include("correctness.jl")
+        end
+        @timedsafetestset "Convergence" begin
+            include("convergence.jl")
+        end
+        @timedsafetestset "State Initialization" begin
+            include("state_init.jl")
+        end
+        @timedsafetestset "Smoothing" begin
+            include("smoothing.jl")
+        end
     end
 
-    @timedtestset "Convergence" begin
-        include("convergence.jl")
-    end
-
-    @timedtestset "Priors" begin
-        include("priors.jl")
-    end
-
-    @timedtestset "Solution" begin
-        include("solution.jl")
-    end
-
-    @timedtestset "DE-stats" begin
-        include("destats.jl")
+    @timedtestset "Interface" begin
+        @timedsafetestset "Solution" begin
+            include("solution.jl")
+        end
+        @timedsafetestset "DE-stats" begin
+            include("destats.jl")
+        end
+        @timedsafetestset "Errors Thrown" begin
+            include("errors_thrown.jl")
+        end
+        @timedsafetestset "Automatic Differentiation" begin
+            include("autodiff.jl")
+        end
+        @timedsafetestset "Second order ODEs" begin
+            include("secondorderode.jl")
+        end
+        @timedsafetestset "DiffEqDevTools.jl Compatibility" begin
+            include("diffeqdevtools.jl")
+        end
     end
 
     @timedtestset "Diffusions" begin
         include("diffusions.jl")
     end
 
-    @timedtestset "State Initialization" begin
-        include("state_init.jl")
-    end
-
-    @timedtestset "Preconditioning" begin
-        include("preconditioning.jl")
-    end
-
-    @timedtestset "Smoothing" begin
-        include("smoothing.jl")
-    end
-
-    @timedtestset "Errors" begin
-        include("errors.jl")
-    end
-
-    @timedtestset "Automatic Differentiation" begin
-        include("autodiff.jl")
-    end
-
-    @timedtestset "Second order ODEs" begin
-        include("secondorderode.jl")
-    end
-
     @timedtestset "Specific Problems" begin
         include("specific_problems.jl")
-    end
-
-    @timedtestset "DiffEqDevTools.jl Compatibility" begin
-        include("diffeqdevtools.jl")
     end
 
     @testset "Aqua.jl" begin
