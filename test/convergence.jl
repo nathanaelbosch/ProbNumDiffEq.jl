86955761206d15c34b836aa73e3a3503411cfbed
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-08-29 17:52:26 +0200

    Make convergence tests cheaper

--- test/convergence.jl
+++ test/convergence.jl
@@ -19,36 +19,36 @@ const dts2 = (1 / 2) .^ (8:-1:6)
 const dts3 = (1 / 2) .^ (12:-1:8)
 @testset "EK0 convergence" begin
     @testset "order=$q" for q in (1, 2, 3, 4)
-        sim = test_convergence(dts1, prob, EK0(order=q))
+        sim = test_convergence(dts1, prob, EK0(order=q, smooth=false), save_everystep=false)
         @test sim.𝒪est[:final] ≈ q + 1 atol = 0.3
-        @test sim.𝒪est[:l2] ≈ q + 1 atol = 0.3
-        @test sim.𝒪est[:l∞] ≈ q + 1 atol = 0.3
+        # @test sim.𝒪est[:l2] ≈ q + 1 atol = 0.3
+        # @test sim.𝒪est[:l∞] ≈ q + 1 atol = 0.3
     end
     @testset "order=$q" for q in (5, 6)
-        sim = test_convergence(dts2, prob, EK0(order=q))
+        sim = test_convergence(dts2, prob, EK0(order=q, smooth=false), save_everystep=false)
         @test sim.𝒪est[:final] ≈ q + 1 atol = 0.3
-        @test sim.𝒪est[:l2] ≈ q + 1 atol = 0.3
-        @test sim.𝒪est[:l∞] ≈ q + 1 atol = 0.3
+        # @test sim.𝒪est[:l2] ≈ q + 1 atol = 0.3
+        # @test sim.𝒪est[:l∞] ≈ q + 1 atol = 0.3
     end
     @testset "order=$q" for q in (7, 8)
-        sim = test_convergence(dts3, prob, EK0(order=q))
+        sim = test_convergence(dts3, prob, EK0(order=q, smooth=false), save_everystep=false)
         @test sim.𝒪est[:final] ≈ q + 1 atol = 1.5
-        @test sim.𝒪est[:l2] ≈ q + 1 atol = 1
-        @test sim.𝒪est[:l∞] ≈ q + 1 atol = 0.3
+        # @test sim.𝒪est[:l2] ≈ q + 1 atol = 1
+        # @test sim.𝒪est[:l∞] ≈ q + 1 atol = 0.3
     end
 end
 
 @testset "EK1 convergence" begin
     @testset "order=$q" for q in (1, 2, 3, 4, 5, 6)
-        sim = test_convergence(dts2, prob, EK0(order=q))
+        sim = test_convergence(dts2, prob, EK0(order=q, smooth=false), save_everystep=false)
         @test sim.𝒪est[:final] ≈ q + 1 atol = 0.3
-        @test sim.𝒪est[:l2] ≈ q + 1 atol = 0.3
-        @test sim.𝒪est[:l∞] ≈ q + 1 atol = 0.3
+        # @test sim.𝒪est[:l2] ≈ q + 1 atol = 0.3
+        # @test sim.𝒪est[:l∞] ≈ q + 1 atol = 0.3
     end
     @testset "order=$q" for q in (7, 8)
-        sim = test_convergence(dts3, prob, EK0(order=q))
+        sim = test_convergence(dts3, prob, EK0(order=q, smooth=false), save_everystep=false)
         @test sim.𝒪est[:final] ≈ q + 1 atol = 1.5
-        @test sim.𝒪est[:l2] ≈ q + 1 atol = 1
-        @test sim.𝒪est[:l∞] ≈ q + 1 atol = 0.3
+        # @test sim.𝒪est[:l2] ≈ q + 1 atol = 1
+        # @test sim.𝒪est[:l∞] ≈ q + 1 atol = 0.3
     end
 end
