d8e70f647b657cdf7f1a36cf8ba794e114212a58
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2023-05-20 13:23:21 +0200

    Fix appxtrue

--- src/devtools.jl
+++ src/devtools.jl
@@ -1,4 +1,4 @@
 # Copied from DiffEqDevTools.jl and modified
 # See also: https://devdocs.sciml.ai/dev/alg_dev/test_problems/
-DiffEqDevTools.appxtrue(sol::ProbODESolution, sol2::TestSolution) =
+DiffEqDevTools.appxtrue(sol::ProbODESolution, sol2) =
     DiffEqDevTools.appxtrue(mean(sol), sol2)
