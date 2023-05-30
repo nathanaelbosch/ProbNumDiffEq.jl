0737907b3bd84ac9874f775286cd82480a5cf77b
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2023-05-30 16:11:57 +0200

    Don't hhide numerical errors by truncating negative eigenvalues

--- src/priors/ioup.jl
+++ src/priors/ioup.jl
@@ -116,9 +116,10 @@ function discretize(p::IOUP, dt::Real)
     A, Q = if p.rate_parameter isa Number
         A_breve, Q_breve = discretize(to_1d_sde(p), dt)
         d = p.wiener_process_dimension
-        # QR_breve = cholesky!(Symmetric(Q_breve)).L'
-        E = eigen(Symmetric(Q_breve))
-        QR_breve = Diagonal(sqrt.(max.(E.values, 0))) * E.vectors'
+        # E = eigen(Symmetric(Q_breve))
+        # QR_breve = Diagonal(sqrt.(max.(E.values, 0))) * E.vectors'
+        # Don't hide numerical issues by truncating negative eigenvalues:
+        QR_breve = cholesky!(Symmetric(Q_breve)).L'
 
         A = kron(I(d), A_breve)
         QR = kron(I(d), QR_breve)
@@ -127,8 +128,10 @@ function discretize(p::IOUP, dt::Real)
     else
         @assert r isa AbstractVector || r isa AbstractMatrix
         A, Q = discretize(to_sde(p), dt)
-        E = eigen(Symmetric(Q))
-        QR = Diagonal(sqrt.(max.(E.values, 0))) * E.vectors'
+        # E = eigen(Symmetric(Q))
+        # QR = Diagonal(sqrt.(max.(E.values, 0))) * E.vectors'
+        # Don't hide numerical issues by truncating negative eigenvalues:
+        QR = cholesky!(Symmetric(Q)).L'
         Q = PSDMatrix(QR)
         A, Q
     end
