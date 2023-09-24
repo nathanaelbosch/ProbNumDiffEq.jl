d968f57f2c5ef2d47aad97550c012800d30cb1ee
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2023-09-24 16:57:31 +0200

    Fix the stiff problem test

--- test/stiff_problem.jl
+++ test/stiff_problem.jl
@@ -5,5 +5,5 @@ using ODEProblemLibrary: prob_ode_vanderpol_stiff
 
 prob = prob_ode_vanderpol_stiff
 appxsol = solve(prob, RadauIIA5())
-sol = solve(prob, EK1(order=3))
+sol = solve(prob, EK1(order=3), abstol=1e-6, reltol=1e-6)
 @test appxsol[end] ≈ sol[end] rtol = 5e-3
