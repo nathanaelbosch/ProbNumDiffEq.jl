4285b686f899645e19bc6f895f68c80807190db2
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2023-10-10 17:22:29 +0200

    Use `elType` in the `projection`

--- src/projection.jl
+++ src/projection.jl
@@ -1,6 +1,6 @@
 function projection(d::Integer, q::Integer, ::Type{elType}=typeof(1.0)) where {elType}
     Proj(deriv) = begin
-        e_i = zeros(q+1, 1)
+        e_i = zeros(elType, q+1, 1)
         if deriv <= q
             e_i[deriv+1] = 1
         end
