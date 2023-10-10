b1ca6f9d0feaea5746017c63ddce51cbb13bf09d
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2023-10-10 18:18:06 +0200

    Add a test to makesure that the previous bug can't happen anymore!

--- test/core/priors.jl
+++ test/core/priors.jl
@@ -1,10 +1,11 @@
+using ProbNumDiffEq: make_transition_matrices!
 using ProbNumDiffEq
 import ProbNumDiffEq as PNDE
 using Test
 using LinearAlgebra
 
-h = rand()
-σ = rand()
+h = 0.1
+σ = 0.1
 
 @testset "General prior API" begin
     for prior in (IWP(2, 3), IOUP(2, 3, 1), Matern(2, 3, 1))
@@ -26,24 +27,64 @@ end
 
     prior = PNDE.IWP(d, q)
 
+    # true sde parameters
+    F = [
+        0 1 0 0 0 0
+        0 0 1 0 0 0
+        0 0 0 0 0 0
+        0 0 0 0 1 0
+        0 0 0 0 0 1
+        0 0 0 0 0 0
+    ]
+    L = [
+        0 0
+        0 0
+        1 0
+        0 0
+        0 0
+        0 1
+    ]
+
+    # true transition matrices
+    AH_22_IBM = [
+        1 h h^2/2 0 0 0
+        0 1 h 0 0 0
+        0 0 1 0 0 0
+        0 0 0 1 h h^2/2
+        0 0 0 0 1 h
+        0 0 0 0 0 1
+    ]
+
+    QH_22_IBM = σ^2 .* [
+        h^5/20 h^4/8 h^3/6 0 0 0
+        h^4/8 h^3/3 h^2/2 0 0 0
+        h^3/6 h^2/2 h 0 0 0
+        0 0 0 h^5/20 h^4/8 h^3/6
+        0 0 0 h^4/8 h^3/3 h^2/2
+        0 0 0 h^3/6 h^2/2 h
+    ]
+
+    # true preconditioned transition matrices
+    AH_22_PRE = [
+        1 2 1 0 0 0
+        0 1 1 0 0 0
+        0 0 1 0 0 0
+        0 0 0 1 2 1
+        0 0 0 0 1 1
+        0 0 0 0 0 1
+    ]
+
+    QH_22_PRE = σ^2 * [
+        1/5 1/4 1/3 0 0 0
+        1/4 1/3 1/2 0 0 0
+        1/3 1/2 1/1 0 0 0
+        0 0 0 1/5 1/4 1/3
+        0 0 0 1/4 1/3 1/2
+        0 0 0 1/3 1/2 1/1
+    ]
+
     @testset "Test SDE" begin
         sde = PNDE.to_sde(prior)
-        F = [
-            0 1 0 0 0 0
-            0 0 1 0 0 0
-            0 0 0 0 0 0
-            0 0 0 0 1 0
-            0 0 0 0 0 1
-            0 0 0 0 0 0
-        ]
-        L = [
-            0 0
-            0 0
-            1 0
-            0 0
-            0 0
-            0 1
-        ]
         @test sde.F ≈ F
         @test sde.L ≈ L
     end
@@ -52,25 +93,7 @@ end
         Ah, Qh = PNDE.discretize(prior, h)
         Qh = PNDE.apply_diffusion(Qh, σ^2)
 
-        AH_22_IBM = [
-            1 h h^2/2 0 0 0
-            0 1 h 0 0 0
-            0 0 1 0 0 0
-            0 0 0 1 h h^2/2
-            0 0 0 0 1 h
-            0 0 0 0 0 1
-        ]
         @test AH_22_IBM ≈ Ah
-
-        QH_22_IBM =
-            σ^2 .* [
-                h^5/20 h^4/8 h^3/6 0 0 0
-                h^4/8 h^3/3 h^2/2 0 0 0
-                h^3/6 h^2/2 h 0 0 0
-                0 0 0 h^5/20 h^4/8 h^3/6
-                0 0 0 h^4/8 h^3/3 h^2/2
-                0 0 0 h^3/6 h^2/2 h
-            ]
         @test QH_22_IBM ≈ Matrix(Qh)
     end
 
@@ -78,27 +101,30 @@ end
         A, Q = PNDE.preconditioned_discretize(prior)
         Qh = PNDE.apply_diffusion(Q, σ^2)
 
-        AH_22_PRE = [
-            1 2 1 0 0 0
-            0 1 1 0 0 0
-            0 0 1 0 0 0
-            0 0 0 1 2 1
-            0 0 0 0 1 1
-            0 0 0 0 0 1
-        ]
-
-        QH_22_PRE =
-            σ^2 * [
-                1/5 1/4 1/3 0 0 0
-                1/4 1/3 1/2 0 0 0
-                1/3 1/2 1/1 0 0 0
-                0 0 0 1/5 1/4 1/3
-                0 0 0 1/4 1/3 1/2
-                0 0 0 1/3 1/2 1/1
-            ]
-
         @test AH_22_PRE ≈ Matrix(A)
         @test QH_22_PRE ≈ Matrix(Qh)
+    end
+
+    @testset "Test `make_transition_matrices!`" begin
+        struct DummyCache <: PNDE.AbstractODEFilterCache
+            d::Any
+            q::Any
+            A::Any
+            Q::Any
+            P::Any
+            PI::Any
+            Ah::Any
+            Qh::Any
+        end
+
+        A, Q, Ah, Qh, P, PI = PNDE.initialize_transition_matrices(prior, h)
+        @test AH_22_PRE ≈ A
+        @test QH_22_PRE ≈ Matrix(PNDE.apply_diffusion(Q, σ^2))
+
+        cache = DummyCache(d, q, A, Q, P, PI, Ah, Qh)
+        make_transition_matrices!(cache, prior, h)
+        @test AH_22_IBM ≈ Ah
+        @test QH_22_IBM ≈ Matrix(PNDE.apply_diffusion(cache.Qh, σ^2))
     end
 end
 
