5e36e9c11ba8f60f5b76dd9cd91deea652718b88
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2023-10-10 16:53:57 +0200

    Fix a _matmul! bug for Kronecker matrices

--- src/kronecker.jl
+++ src/kronecker.jl
@@ -6,7 +6,7 @@ _matmul!(A::KP, B::KP, C::KP) = begin
     return A
 end
 _matmul!(A::KP, B::KP, C::KP, a, b) = begin
-    _matmul!(A.A, B.A, C.A, a, b)
+    _matmul!(A.A, B.A, C.A)
     _matmul!(A.B, B.B, C.B, a, b)
     return A
 end
