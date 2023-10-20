bab0e14552638bd8f16f96c53fad362ce7b68b8b
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2023-10-20 10:01:23 +0200

    Add two missing return statements

--- src/filtering/markov_kernel.jl
+++ src/filtering/markov_kernel.jl
@@ -116,7 +116,7 @@ function marginalize_cov!(
     _D = size(_x_out.Σ, 1)
     _C_DxD = view(C_DxD, 1:_D, 1:_D)
     _C_3DxD = view(C_3DxD, 1:3*_D, 1:_D)
-    marginalize_cov!(_x_out, _x_curr, _K; C_DxD=_C_DxD, C_3DxD=_C_3DxD)
+    return marginalize_cov!(_x_out, _x_curr, _K; C_DxD=_C_DxD, C_3DxD=_C_3DxD)
 end
 
 """
@@ -229,5 +229,5 @@ function compute_backward_kernel!(
     _K = AffineNormalKernel(K.A.B, K.b, PSDMatrix(K.C.R.B))
     _D = size(_Kout.A, 1)
     _C_DxD = view(C_DxD, 1:_D, 1:_D)
-    compute_backward_kernel!(_Kout, _x_pred, _x, _K; C_DxD=_C_DxD, diffusion=diffusion)
+    return compute_backward_kernel!(_Kout, _x_pred, _x, _K; C_DxD=_C_DxD, diffusion=diffusion)
 end
