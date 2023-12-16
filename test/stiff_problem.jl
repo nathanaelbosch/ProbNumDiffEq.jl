42b53cd8433615136b16f742b9bec7a097ac600a
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2023-12-16 09:18:00 -0600

    Fix a typo that I introdced in my last committ

--- test/stiff_problem.jl
+++ test/stiff_problem.jl
@@ -7,4 +7,4 @@ prob = prob_ode_vanderpol_stiff
 appxsol = solve(prob, RadauIIA5())
 sol = solve(prob, EK1(order=3))
 @test appxsol[end] ≈ sol[end] rtol = 5e-3
-@test appxsol(0.5) ≈ sol(0.5) rtol = 5e-3
+@test appxsol(0.5) ≈ sol(0.5).μ rtol = 5e-3
