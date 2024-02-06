e19bb3ebe2e92a3084096234d0904043432d4fad
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2024-02-06 12:18:40 +0100

    Actually remove the filtering likelihood from the dalton file

--- src/data_likelihoods/dalton.jl
+++ src/data_likelihoods/dalton.jl
@@ -9,6 +9,7 @@ function dalton_data_loglik(
     data::NamedTuple{(:t, :u)},
     kwargs...
 )
+
     if alg.smooth
         str = "The passed algorithm performs smoothing, but `dalton_nll` can be used without. " *
             "You might want to set `smooth=false` to imprpove performance."
@@ -52,41 +53,3 @@ function dalton_data_loglik(
     return dalton_ll
 end
 
-function filtering_data_loglik(
-    prob::SciMLBase.AbstractODEProblem,
-    alg::AbstractEK,
-    args...;
-    # observation model
-    observation_matrix=I,
-    observation_noise_cov::Union{Number, AbstractMatrix},
-    # data
-    data::NamedTuple{(:t, :u)},
-    kwargs...
-)
-    if alg.smooth
-        str = "The passed algorithm performs smoothing, but `dalton_nll` can be used without. " *
-            "You might want to set `smooth=false` to imprpove performance."
-        @warn str
-    end
-    if :tstops in keys(kwargs)
-        str = "The passed `tstops` argument will be extended with the observation locations `data.t`."
-        @warn str
-    end
-    tstops = union(data.t, get(kwargs, :tstops, []))
-
-    data_ll = DataUpdateLogLikelihood(zero(eltype(prob.p)))
-
-    cb = DataUpdateCallback(
-        data; observation_matrix, observation_noise_cov,
-        loglikelihood=data_ll)
-
-    solve(
-        prob, alg, args...;
-        callback=cb,
-        save_everystep=false,
-        tstops,
-        kwargs...,
-    )
-
-    return data_ll.ll
-end
