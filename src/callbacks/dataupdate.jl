686badde22a03d6bd053fa3d7ad6162bff60339f
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2024-02-07 12:59:42 +0100

    Misc updates to the data update callback

--- src/callbacks/dataupdate.jl
+++ src/callbacks/dataupdate.jl
@@ -16,34 +16,34 @@ function DataUpdateCallback(
 
         o = length(val)
 
-        @unpack K1, C_DxD, E0, C_dxd, C_Dxd, C_d = integ.cache
+
+        x = integ.cache.x
+        H = observation_matrix * integ.cache.E0
+        obs_mean = H * x.μ - val
+
+        R = if observation_noise_cov isa PSDMatrix
+            observation_noise_cov
+        elseif observation_noise_cov isa Number
+            PSDMatrix(sqrt(observation_noise_cov) * Eye(o))
+        elseif observation_noise_cov isa UniformScaling
+            PSDMatrix(sqrt(observation_noise_cov.λ) * Eye(o))
+        else
+            PSDMatrix(cholesky(observation_noise_cov).U)
+        end
+
+        # _A = x.Σ.R * H'
+        # obs_cov = _A'_A + R
+        obs_cov = PSDMatrix(qr!([x.Σ.R * H'; R.R]).R)
+        obs = Gaussian(obs_mean, obs_cov)
+
+        @unpack x_tmp,K1, C_DxD, C_dxd, C_Dxd, C_d = integ.cache
         K1 = view(K1, :, 1:o)
         C_dxd = view(C_dxd, 1:o, 1:o)
         C_Dxd = view(C_Dxd, :, 1:o)
         C_d = view(C_d, 1:o)
+        _x = copy!(x_tmp, x)
+        _, ll = update!(x, _x, obs, H, K1, C_Dxd, C_DxD, C_dxd, C_d; R=R)
 
-        x = integ.cache.x
-
-        H = observation_matrix * E0
-
-        obs_mean = H * x.μ - val
-
-        R = if observation_noise_cov isa Number
-            observation_noise_cov * Eye(o)
-        else
-            observation_noise_cov
-        end
-
-        obs_cov = if observation_noise_cov isa PSDMatrix
-            PSDMatrix(qr!([x.Σ.R * H'; observation_noise_cov.R]).R)
-        else
-            _A = x.Σ.R * H'
-            _A'_A + R
-        end
-        obs = Gaussian(obs_mean, obs_cov)
-
-        _x = copy!(integ.cache.x_tmp, x)
-        _, ll = update!(x, _x, obs, H, K1, C_Dxd, C_DxD, C_dxd, C_d)
         if !isnothing(loglikelihood)
             loglikelihood.ll += ll
         end
