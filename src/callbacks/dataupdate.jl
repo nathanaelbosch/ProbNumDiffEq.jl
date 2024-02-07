f81799c36af531b07c847f4426dc5fb28fba80f3
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2024-02-07 14:17:56 +0100

    Add docstrings for DataUpdateLogLikelihood and DataUpdateCallback

--- src/callbacks/dataupdate.jl
+++ src/callbacks/dataupdate.jl
@@ -1,13 +1,48 @@
+"""
+$(TYPEDEF)
+
+This is just a container for the data log-likelihood such that it can be computed in the
+[`DataUpdateCallback`](@ref) and is accessible on the outside, by mutating the `ll` field.
+"""
 mutable struct DataUpdateLogLikelihood{T<:Number}
     ll::T
 end
 
-using LinearAlgebra: NumberArray
+@doc raw"""
+    DataUpdateCallback(
+        data::NamedTuple{(:t, :u)};
+        observation_matrix=I,
+        observation_noise_cov,
+        loglikelihood::Union{DataUpdateLogLikelihood,Nothing}=nothing,
+        save_positions=(false, false),
+        kwargs...
+    )
+
+Update the state accoding to the linear observations during the filter pass.
+
+`DataUpdateCallback` returns a `DiffEqCallbacks.PresetTimeCallback`, which (i) adjusts the
+`tstops` to include the observation times (`data.t`) and (ii) whenever a time step
+coincides with the data locations, it updates the state on the data point according to
+the observation model
+```math
+\begin{aligned}
+y(t) &= H x(t) + \varepsilon(t), \quad \varepsilon(t) \sim \mathcal{N}(0, R),
+\end{aligned}
+```
+where ``H`` is the observation matrix (`observation_matrix`) and
+``R`` is the observation noise covariance (`observation_noise_cov`).
+
+By passing a [`DataUpdateLogLikelihood`](@ref) object with the `loglikelihood` keyword
+argument, the log-likelihood of the data is computed and stored in the `ll` field, and can
+be accessed after call to `solve`.
+"""
 function DataUpdateCallback(
     data::NamedTuple{(:t, :u)};
     observation_matrix=I,
     observation_noise_cov,
     loglikelihood::Union{DataUpdateLogLikelihood,Nothing}=nothing,
+    save_positions=(false, false),
+    kwargs...
 )
     function affect!(integ)
         times, values = data.t, data.u
@@ -47,5 +82,5 @@ function DataUpdateCallback(
             loglikelihood.ll += ll
         end
     end
-    return PresetTimeCallback(data.t, affect!; save_positions=(false, false))
+    return PresetTimeCallback(data.t, affect!; save_positions, kwargs...)
 end
